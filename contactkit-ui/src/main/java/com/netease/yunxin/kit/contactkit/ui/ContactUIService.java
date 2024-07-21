3cf04513056b790b9bc40feec545f0462af09bb3
XTZJ-2024ESZKOM\Administrator <756672423@qq.com>
2024-07-21 22:11:36 +0800

    搜索

--- contactkit-ui/src/main/java/com/netease/yunxin/kit/contactkit/ui/ContactUIService.java
+++ contactkit-ui/src/main/java/com/netease/yunxin/kit/contactkit/ui/ContactUIService.java
@@ -26,6 +26,7 @@ import com.netease.yunxin.kit.contactkit.ui.normal.selector.ContactSelectorActiv
 import com.netease.yunxin.kit.contactkit.ui.normal.team.TeamListActivity;
 import com.netease.yunxin.kit.contactkit.ui.normal.userinfo.UserInfoActivity;
 import com.netease.yunxin.kit.contactkit.ui.normal.verify.VerifyListActivity;
+import com.netease.yunxin.kit.contactkit.ui.search.SearchNewActivity;
 import com.netease.yunxin.kit.corekit.im.utils.RouterConstant;
 import com.netease.yunxin.kit.corekit.route.XKitRouter;
 import com.yaoxin.appbase.net.Constant;
@@ -57,6 +58,7 @@ public class ContactUIService extends ChatService {
     XKitRouter.registerRouter(RouterConstant.PATH_MY_NOTIFICATION_PAGE, VerifyListActivity.class);
     XKitRouter.registerRouter(RouterConstant.PATH_CONTACT_PAGE, ContactActivity.class);
     XKitRouter.registerRouter(RouterConstant.PATH_GLOBAL_SEARCH_PAGE, GlobalSearchActivity.class);
+    XKitRouter.registerRouter("SearchNewActivity", SearchNewActivity.class);
 
     XKitRouter.registerRouter(
         RouterConstant.PATH_FUN_CONTACT_SELECTOR_PAGE, FunContactSelectorActivity.class);
---
+++ contactkit-ui/src/main/java/com/netease/yunxin/kit/contactkit/ui/search/SearchNewActivity.java
@@ -0,0 +1,109 @@
+package com.netease.yunxin.kit.contactkit.ui.search;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.chad.library.adapter4.BaseQuickAdapter;
+import com.netease.yunxin.kit.contactkit.ui.databinding.FunSearchActivityNewBinding;
+import com.netease.yunxin.kit.contactkit.ui.normal.contact.adapter.ContactUserListAdapter;
+import com.netease.yunxin.kit.corekit.im.utils.RouterConstant;
+import com.netease.yunxin.kit.corekit.route.XKitRouter;
+import com.yaoxin.appbase.activity.BaseActivity;
+import com.yaoxin.appbase.model.GroupInfoBean;
+import com.yaoxin.appbase.net.Constant;
+import com.yaoxin.appbase.utils.DataUtil;
+
+import java.util.ArrayList;
+
+public class SearchNewActivity extends BaseActivity implements View.OnClickListener {
+    FunSearchActivityNewBinding binding;
+    ContactUserListAdapter adapter = new ContactUserListAdapter();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = FunSearchActivityNewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.funSearchActivityNav.addCloseImageButton().setOnClickListener(this);
+
+        binding.funSearchActivityRv.setLayoutManager(new LinearLayoutManager(this));
+        binding.funSearchActivityRv.setAdapter(adapter);
+
+        adapter._isSearch = true;
+        Context that = this;
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener<GroupInfoBean>() {
+            @Override
+            public void onClick(@NonNull BaseQuickAdapter<GroupInfoBean, ?> baseQuickAdapter, @NonNull View view, int i) {
+                if (baseQuickAdapter.getItemViewType(i) == Constant.RECYCLE_VIEW_ITEM) {
+                    XKitRouter.withKey(RouterConstant.PATH_FUN_CHAT_SETTING_PAGE)
+                            .withParam(RouterConstant.CHAT_ID_KRY, baseQuickAdapter.getItem(i ).userId)
+                            .withParam("type", "1")
+                            .withContext(that)
+                            .navigate();
+                }
+            }
+        });
+        binding.funSearchActivityEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String string = s.toString();
+                if (string.isEmpty()) {
+                    adapter.contacts = new ArrayList<>();
+                    adapter.setItems(new ArrayList<>());
+                    adapter.notifyDataSetChanged();
+                    binding.funSearchActivityEmptyLl.setVisibility(View.GONE);
+
+                } else {
+                    ArrayList<GroupInfoBean> tempArr = new ArrayList<>();
+                    for (GroupInfoBean temp :
+                            DataUtil.getFriendInfoList()) {
+                        if (temp.remark != null && !temp.remark.isEmpty()) {
+                            if (temp.remark.contains(string)) {
+                                tempArr.add(temp);
+                            }
+                        } else {
+                            if (temp.name.contains(string)) {
+                                tempArr.add(temp);
+                            }
+                        }
+
+                    }
+                    if (tempArr.isEmpty()) {
+                        binding.funSearchActivityEmptyLl.setVisibility(View.VISIBLE);
+                    } else {
+                        binding.funSearchActivityEmptyLl.setVisibility(View.GONE);
+
+                    }
+                    adapter.contacts = tempArr;
+                    adapter.setItems(tempArr);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == binding.funSearchActivityNav.addCloseImageButton()) {
+            finish();
+        }
+    }
+
+}
