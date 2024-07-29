9e2dde9e3711456a1a30ede410679dfcea42f2c8
XTZJ-2024ESZKOM\Administrator <756672423@qq.com>
2024-07-29 23:01:26 +0800

    loading  突然停顿修改
    (cherry picked from commit ed339be076e24c9445e7bece0d81aad1ceff725b)
    (cherry picked from commit cc125b7afe13b7318c0635b67478050eec07aeac)

--- teamkit-ui/src/main/java/com/netease/yunxin/kit/teamkit/ui/fun/activity/FunTeamSettingNewActivity.java
+++ teamkit-ui/src/main/java/com/netease/yunxin/kit/teamkit/ui/fun/activity/FunTeamSettingNewActivity.java
@@ -237,7 +237,7 @@ public class FunTeamSettingNewActivity extends BaseActivity implements View.OnCl
         bean.page = page +"";
         bean.pageNo ="100";
 
-        LoadingDialog.showDialog(getSupportFragmentManager(),"加载中");
+//        LoadingDialog.showDialog(getSupportFragmentManager(),"加载中");
         HttpUtil.apiW().group_groupUserListPost(bean)
                 .enqueue(new CommonCallback<NetData>() {
                     @Override
@@ -251,23 +251,27 @@ public class FunTeamSettingNewActivity extends BaseActivity implements View.OnCl
                             if (tempList.size() == 100) {
                                 _requestPeople((page + 1));
                             } else {
+                                LoadingDialog.dismissDialog();
                                 requestYunXin();
                                 updateUI();
                             }
                         } else {
+                            LoadingDialog.dismissDialog();
                             requestYunXin();
                             updateUI();
+
                         }
                     }
 
                     @Override
                     public void Failure(Call<NetData> call, Throwable t) {
+                        LoadingDialog.dismissDialog();
                     }
 
                     @Override
                     public void end() {
                         super.end();
-                        LoadingDialog.dismissDialog();
+
                     }
                 });
     }
@@ -289,14 +293,14 @@ public class FunTeamSettingNewActivity extends BaseActivity implements View.OnCl
 
                     @Override
                     public void Failure(Call<NetData> call, Throwable t) {
-
-                    }
-
-                    @Override
-                    public void end() {
-                        super.end();
                         LoadingDialog.dismissDialog();
                     }
+
+//                    @Override
+//                    public void end() {
+//                        super.end();
+//
+//                    }
                 });
     }
 
