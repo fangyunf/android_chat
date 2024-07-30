79ef9b9f10a3927d3acbb099dc683e15dabec38f
allen <>
2024-07-30 08:51:38 +0800

    1

--- app/src/main/java/com/turunsi/yaoxin/login/LoginActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/login/LoginActivity.java
@@ -88,8 +88,8 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
         changeTitleWithType(0);
         if (BuildConfig.DEBUG) {
 
-        binding.activityLoginTf1.viewRoundTfEt.setText("13814347472");
-        binding.activityLoginTf3.viewRoundTfEt.setText("wzy901017");
+        binding.activityLoginTf1.viewRoundTfEt.setText("18616821287");
+        binding.activityLoginTf3.viewRoundTfEt.setText("12345678a");
         }
     }
 
