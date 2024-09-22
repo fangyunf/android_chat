4158906903695bd352f694498bc6c280d4cbec8f
allen <756672423@qq.com>
2024-09-22 22:59:14 +0800

    金蟾

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,11 +18,11 @@ android {
         }
     }
     defaultConfig {
-        applicationId = "com.turunsi.jinzita"
+        applicationId = "com.turunsi.jinchan"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "2.0.17"
+        versionName = "1.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
