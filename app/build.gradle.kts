95c0162c514d115b2978bb306e19853c25f9e5c6
allen <>
2024-10-09 15:53:45 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         }
     }
     defaultConfig {
-        applicationId = "com.turunsi.balishijia"
+        applicationId = "com.turunsi.sixin"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
