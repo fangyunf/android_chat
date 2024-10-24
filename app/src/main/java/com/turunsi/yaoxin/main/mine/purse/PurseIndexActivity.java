9f558d8402204415ea8dc43ab1908e8fe367c1b4
allen <>
2024-10-24 15:01:16 +0800

    Revert "换icon 启动图"
    This reverts commit d2442a6aba61aa7c3ce217807792a7f8ce371b57.

--- app/src/main/java/com/turunsi/yaoxin/main/mine/purse/PurseIndexActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/main/mine/purse/PurseIndexActivity.java
@@ -130,13 +130,12 @@ public class PurseIndexActivity extends BaseActivity implements View.OnClickList
 //            ToastUtils.toastMsg("敬请期待");
 //        }
         else if (v == binding.activityMinePurseIndexSmrz) {
-            ToastUtils.toastMsg("已实名成功");
-//            if (!Constant.isRunningRealName) {
-//                Constant.isRunningRealName = true;
-//                XKitRouter.withKey(Constant.RealName_Router)
-//                        .withContext(AppProxy.getInstance().getContext())
-//                        .navigate();
-//            }
+            if (!Constant.isRunningRealName) {
+                Constant.isRunningRealName = true;
+                XKitRouter.withKey(Constant.RealName_Router)
+                        .withContext(AppProxy.getInstance().getContext())
+                        .navigate();
+            }
         }
 //        else if (v == binding.activityMinePurseIndexCell8.itemPurseIndexCellRl) {
 //            HashMap map = new HashMap();
