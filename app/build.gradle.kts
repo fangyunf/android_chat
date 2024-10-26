17c4d5c21ba599af407e6c01ab225833dafb6e63
allen <756672423@qq.com>
2024-10-27 00:13:04 +0800

    金蟾改名回头客

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         }
     }
     defaultConfig {
-        applicationId = "com.turunsi.jinchan"
+        applicationId = "com.turunsi.huitouke"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
