5b3841db5ab60f0ce14807d1269b7f58afc0ca41
allen <756672423@qq.com>
2024-11-11 18:12:47 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.13"
+        versionName = "1.0.2"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -15,8 +15,8 @@ public class Constant {
 //    public static final String ENCODE_KEY = "frxsecretpadding";
 //    public static final String MSG_ENCODE_KEY = "carefullanimchat";
 
-    public static final String BASE_URL = "http://106.14.67.43/";
-    public static final String BASE_URL_H5 = "http://106.14.67.43";
+    public static final String BASE_URL = "http://www.shiyisui.com/";
+    public static final String BASE_URL_H5 = "http://www.shiyisui.com";
     public static final String ENCODE_KEY = "wesecretpaddingg";
     public static final String MSG_ENCODE_KEY = "wallstreetimchat";
 
