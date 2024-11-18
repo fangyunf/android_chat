32869c83e7b175f5892de9fc25662a4a6ddb2c1b
allen <756672423@qq.com>
2024-11-18 21:52:26 +0800

    商城修改1

--- contactkit-ui/src/main/java/com/netease/yunxin/kit/contactkit/ui/normal/contact/ShopNewFragment.java
+++ contactkit-ui/src/main/java/com/netease/yunxin/kit/contactkit/ui/normal/contact/ShopNewFragment.java
@@ -11,9 +11,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.chad.library.adapter4.BaseQuickAdapter;
+import com.netease.yunxin.kit.common.utils.SizeUtils;
 import com.netease.yunxin.kit.contactkit.ui.databinding.ShopNewFragmentBinding;
+import com.netease.yunxin.kit.contactkit.ui.normal.contact.adapter.ShoprListAdapter;
 import com.yaoxin.appbase.fragment.BaseFragment;
+import com.yaoxin.appbase.model.GroupInfoBean;
+import com.yaoxin.appbase.view.CommonGridSpacingItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * contact page
@@ -21,12 +30,33 @@ import com.yaoxin.appbase.fragment.BaseFragment;
 public class ShopNewFragment extends BaseFragment {
     private final String TAG = "ContactFragment";
     private ShopNewFragmentBinding binding;
+    ShoprListAdapter adapter = new ShoprListAdapter();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = ShopNewFragmentBinding.inflate(inflater, container, false);
 
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        binding.shopNewFragmentRv.setLayoutManager(gridLayoutManager);
+        CommonGridSpacingItemDecoration gridSpacingItemDecoration =
+                new CommonGridSpacingItemDecoration(2, SizeUtils.dp2px(10), false);
+        binding.shopNewFragmentRv.addItemDecoration(gridSpacingItemDecoration);
+        List<GroupInfoBean> data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            GroupInfoBean groupInfoBean = new GroupInfoBean();
+            groupInfoBean.name = "name" + i;
+            groupInfoBean.price = i;
+            data.add(groupInfoBean);
+        }
+        adapter.setItems(data);
+        binding.shopNewFragmentRv.setAdapter(adapter);
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener<GroupInfoBean>() {
+            @Override
+            public void onClick(@NonNull BaseQuickAdapter<GroupInfoBean, ?> baseQuickAdapter, @NonNull View view, int i) {
+                SubmitOrderActivity.start(SubmitOrderActivity.class, requireActivity(), null);
+            }
+        });
         return binding.getRoot();
     }
 
---
+++ contactkit-ui/src/main/java/com/netease/yunxin/kit/contactkit/ui/normal/contact/SubmitOrderActivity.java
@@ -0,0 +1,66 @@
+// Copyright (c) 2022 NetEase, Inc. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+package com.netease.yunxin.kit.contactkit.ui.normal.contact;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.netease.nimlib.sdk.misc.DirCacheFileType;
+import com.netease.yunxin.kit.contactkit.ui.databinding.ActivitySubmitOrderBinding;
+import com.netease.yunxin.kit.corekit.im.IMKitClient;
+import com.netease.yunxin.kit.corekit.im.provider.FetchCallback;
+import com.netease.yunxin.kit.corekit.im.repo.MiscRepo;
+import com.yaoxin.appbase.activity.BaseActivity;
+import com.yaoxin.appbase.model.NetData;
+import com.yaoxin.appbase.net.CommonCallback;
+import com.yaoxin.appbase.net.HttpUtil;
+import com.yaoxin.appbase.utils.DataUtil;
+import com.yaoxin.appbase.utils.DialogAlertUtil;
+import com.yaoxin.appbase.utils.ToastUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class SubmitOrderActivity extends BaseActivity implements View.OnClickListener {
+
+  private ActivitySubmitOrderBinding viewBinding;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+//    changeStatusBarColor(R.color.color_e9eff5);
+    viewBinding = ActivitySubmitOrderBinding.inflate(getLayoutInflater());
+    viewBinding.activitySubmitOrderChooseAddressLl.setOnClickListener(this);
+    setContentView(viewBinding.getRoot());
+    initView();
+  }
+
+  private void initView() {
+
+    viewBinding.activitySubmitOrderNav.addCloseImageButton().setOnClickListener(this);
+
+  }
+
+  @Override
+  public void onClick(View v) {
+   if (v == viewBinding.activitySubmitOrderNav.addCloseImageButton()) {
+      finish();
+
+    }
+   if (v == viewBinding.activitySubmitOrderChooseAddressLl) {
+      OrderAddressActivity.start(OrderAddressActivity.class, this, null);
+
+    }
+  }
+
+}
