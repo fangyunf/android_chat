b7e778a6cf079e0986fdc0bfbdb7a9af714998a0
allen <>
2024-12-11 09:30:38 +0800

    删除聊天记录

--- chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/fun/page/FunChatSettingActivity.java
+++ chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/fun/page/FunChatSettingActivity.java
@@ -26,6 +26,7 @@ import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.msg.MsgService;
 import com.netease.nimlib.sdk.msg.constant.DeleteTypeEnum;
 import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
+import com.netease.nimlib.sdk.msg.model.RecentContact;
 import com.netease.nimlib.sdk.msg.model.StickTopSessionInfo;
 import com.netease.yunxin.kit.alog.ALog;
 import com.netease.yunxin.kit.chatkit.repo.ConversationRepo;
@@ -257,7 +258,10 @@ public class FunChatSettingActivity extends BaseActivity implements View.OnClick
                     @Override
                     public void clickType(int type) {
                         if (type == 1) {
+
+
                             NIMClient.getService(MsgService.class).clearChattingHistory(userBean.userId,SessionTypeEnum.P2P);
+                            NIMClient.getService(MsgService.class).clearServerHistory(userBean.userId,SessionTypeEnum.P2P);
 
                             EventBus.getDefault().post(new BaseEvent("clearP2PMessageList"));
                         }
