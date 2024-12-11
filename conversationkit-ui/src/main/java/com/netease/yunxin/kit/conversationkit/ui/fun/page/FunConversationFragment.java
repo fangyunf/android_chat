7f724cec2cd3a9d9faf64ea2499bdd8e886f48c5
allen <>
2024-12-11 09:30:38 +0800

    删除聊天记录
    (cherry picked from commit b7e778a6cf079e0986fdc0bfbdb7a9af714998a0)
    (cherry picked from commit 1c6b4b35f660cf622c829b0119c97b895a471b17)
    (cherry picked from commit 12a9cebf23bf9d3b49a077d542aa5dd96007f264)
    (cherry picked from commit 968cca536a5d55f13d12c28711700e20d9beeceb)
    (cherry picked from commit e021c9f18fef77dda234808211f2e209538148bf)
    (cherry picked from commit 62aa9217e2221494de35cb76825a3bb0a1a6e0cb)
    (cherry picked from commit 7e1b220cc954839b50ddd627db3bad96f9909ef9)

--- conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
+++ conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
@@ -238,6 +238,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
                             @Override
                             public void Successful(Call<NetData> call, Response<NetData> response, NetData body) {
                               NIMClient.getService(MsgService.class).clearChattingHistory(kefuId,SessionTypeEnum.P2P);
+                              NIMClient.getService(MsgService.class).clearServerHistory(kefuId,SessionTypeEnum.P2P);
                             }
 
                             @Override
@@ -277,6 +278,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
       @Override
       public void onSuccess(Void param) {
         NIMClient.getService(MsgService.class).clearChattingHistory(account,SessionTypeEnum.P2P);
+        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.P2P);
         // 保存成功
       }
 
@@ -313,6 +315,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
       @Override
       public void onSuccess(Void param) {
         NIMClient.getService(MsgService.class).clearChattingHistory(account,SessionTypeEnum.Team);
+        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.Team);
         // 保存成功
       }
 
