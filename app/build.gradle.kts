38c9daf2ff11d30b60e314255070232d1ba36dc7
allen <>
2024-12-13 16:50:42 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.36"
+        versionName = "1.0.37"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
