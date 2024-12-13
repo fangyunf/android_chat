ac364346c5829772a214d6e65803495a80c8b92a
allen <>
2024-12-13 11:12:13 +0800

    发送收藏图片 文字

--- app/src/main/java/com/turunsi/yaoxin/main/mine/collection/adapter/CollectionListAdapter.java
+++ app/src/main/java/com/turunsi/yaoxin/main/mine/collection/adapter/CollectionListAdapter.java
@@ -1,7 +1,10 @@
 package com.turunsi.yaoxin.main.mine.collection.adapter;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,7 @@ import com.turunsi.yaoxin.R;
 import com.turunsi.yaoxin.main.mine.collection.bean.CollectionListBean;
 import com.turunsi.yaoxin.main.mine.order.bean.OrderListBean;
 import com.yaoxin.appbase.utils.AESUtil;
+import com.yaoxin.appbase.utils.GlideUtil;
 import com.yaoxin.appbase.utils.TimeUtil;
 
 public class CollectionListAdapter extends BaseQuickAdapter<CollectInfo, QuickViewHolder> {
@@ -25,9 +29,22 @@ public class CollectionListAdapter extends BaseQuickAdapter<CollectInfo, QuickVi
         }catch (Exception e) {
 
         }
-        quickViewHolder.setText(R.id.item_collection_list_cell_content_tv,content)
-                .setText(R.id.item_collection_list_cell_date_tv, TimeUtil.stampToDate(orderListBean.getCreateTime() +""));
-//                .setText(R.id.item_collection_list_cell_user_tv,orderListBean.)
+        ImageView imageView = quickViewHolder.findView(R.id.item_collection_list_cell_content_iv);
+        LinearLayout textLl = quickViewHolder.findView(R.id.item_collection_list_cell_text_ll);
+        if (imageView == null || textLl == null) {
+            return;
+        }
+        imageView.setVisibility(View.GONE);
+        textLl.setVisibility(View.GONE);
+        if (orderListBean.getType() == 1) {
+            imageView.setVisibility(View.VISIBLE);
+            GlideUtil.yh_loadImage(getContext(),imageView,orderListBean.getData());
+        } else {
+            textLl.setVisibility(View.VISIBLE);
+            quickViewHolder.setText(R.id.item_collection_list_cell_content_tv,content);
+        }
+
+        quickViewHolder.setText(R.id.item_collection_list_cell_date_tv, TimeUtil.stampToDate(orderListBean.getCreateTime() +""));
     }
 
     @NonNull
