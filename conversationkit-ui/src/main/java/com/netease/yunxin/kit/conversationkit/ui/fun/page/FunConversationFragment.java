68a974584b817cd3e1796562b2f8c2ff55e1d1d5
allen <>
2024-12-14 14:55:42 +0800

    1
    (cherry picked from commit 3d43f0a39b1492c0b58f595e6859db4f3046be44)
    (cherry picked from commit d82888e5fa589b435cf85d2935bce2c40d53772c)
    (cherry picked from commit 230b162c4575d40376efadae3e037505e9a9a317)
    (cherry picked from commit fe1785554f0519653c7085a8fa08b606fceb97fc)
    (cherry picked from commit e6f78054983310aa2b053edd4dc5b18a2e3e8715)

--- conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
+++ conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
@@ -238,7 +238,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
                             @Override
                             public void Successful(Call<NetData> call, Response<NetData> response, NetData body) {
                               NIMClient.getService(MsgService.class).clearChattingHistory(kefuId,SessionTypeEnum.P2P);
-                              NIMClient.getService(MsgService.class).clearServerHistory(kefuId,SessionTypeEnum.P2P);
+//                              NIMClient.getService(MsgService.class).clearServerHistory(kefuId,SessionTypeEnum.P2P);
                             }
 
                             @Override
@@ -278,7 +278,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
       @Override
       public void onSuccess(Void param) {
         NIMClient.getService(MsgService.class).clearChattingHistory(account,SessionTypeEnum.P2P);
-        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.P2P);
+//        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.P2P);
         // 保存成功
       }
 
@@ -315,7 +315,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
       @Override
       public void onSuccess(Void param) {
         NIMClient.getService(MsgService.class).clearChattingHistory(account,SessionTypeEnum.Team);
-        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.Team);
+//        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.Team);
         // 保存成功
       }
 
