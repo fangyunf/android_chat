d82888e5fa589b435cf85d2935bce2c40d53772c
allen <>
2024-12-14 14:55:42 +0800

    1
    (cherry picked from commit 3d43f0a39b1492c0b58f595e6859db4f3046be44)

--- conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
+++ conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
@@ -333,7 +333,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
                             @Override
                             public void Successful(Call<NetData> call, Response<NetData> response, NetData body) {
                               NIMClient.getService(MsgService.class).clearChattingHistory(kefuId,SessionTypeEnum.P2P);
-                              NIMClient.getService(MsgService.class).clearServerHistory(kefuId,SessionTypeEnum.P2P);
+//                              NIMClient.getService(MsgService.class).clearServerHistory(kefuId,SessionTypeEnum.P2P);
                             }
 
                             @Override
@@ -373,7 +373,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
       @Override
       public void onSuccess(Void param) {
         NIMClient.getService(MsgService.class).clearChattingHistory(account,SessionTypeEnum.P2P);
-        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.P2P);
+//        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.P2P);
         // 保存成功
       }
 
@@ -410,7 +410,7 @@ public class FunConversationFragment extends ConversationBaseFragment {
       @Override
       public void onSuccess(Void param) {
         NIMClient.getService(MsgService.class).clearChattingHistory(account,SessionTypeEnum.Team);
-        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.Team);
+//        NIMClient.getService(MsgService.class).clearServerHistory(account,SessionTypeEnum.Team);
         // 保存成功
       }
 
