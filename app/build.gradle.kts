2ee3138e6e75ef77b08927ad68bec3026ada4bb9
allen <756672423@qq.com>
2024-12-21 22:16:25 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.39"
+        versionName = "1.0.40"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
