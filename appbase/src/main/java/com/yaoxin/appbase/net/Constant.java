92f1ec3e58e548a6bd54b586fcf83fff7a0bcfa1
allen <756672423@qq.com>
2024-12-28 22:06:33 +0800

    1

--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -15,8 +15,8 @@ public class Constant {
 //    public static final String ENCODE_KEY = "frxsecretpadding";
 //    public static final String MSG_ENCODE_KEY = "carefullanimchat";
 
-    public static final String BASE_URL = "http://ml.shijiezhixin.cn/";
-    public static final String BASE_URL_H5 = "http://ml.shijiezhixin.cn";
+    public static final String BASE_URL = "http://118.31.18.170:10125/";
+    public static final String BASE_URL_H5 = "http://118.31.18.170:10125";
     public static final String ENCODE_KEY = "wesecretpaddingg";
     public static final String MSG_ENCODE_KEY = "wallstreetimchat";
 
