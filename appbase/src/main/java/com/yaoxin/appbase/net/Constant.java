f47a0709c8f6532361b39ebc644a4bcf5d9e4299
allen <>
2025-02-13 15:10:53 +0800

    1

--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -15,8 +15,8 @@ public class Constant {
 //    public static final String ENCODE_KEY = "frxsecretpadding";
 //    public static final String MSG_ENCODE_KEY = "carefullanimchat";
 
-    public static final String BASE_URL = "http://8.129.97.183:10125/";
-    public static final String BASE_URL_H5 = "http://8.129.97.183:10125";
+    public static final String BASE_URL = "https://ml.shijiezhixin.cn/";
+    public static final String BASE_URL_H5 = "https://ml.shijiezhixin.cn";
     public static final String ENCODE_KEY = "wesecretpaddingg";
     public static final String MSG_ENCODE_KEY = "wallstreetimchat";
 
