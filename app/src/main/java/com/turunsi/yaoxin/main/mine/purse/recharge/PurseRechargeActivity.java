cae59ac2fd7003df6a96a2ca71ab8a20b38aaec4
allen <>
2025-03-11 09:44:03 +0800

    1

--- app/src/main/java/com/turunsi/yaoxin/main/mine/purse/recharge/PurseRechargeActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/main/mine/purse/recharge/PurseRechargeActivity.java
@@ -38,7 +38,34 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Response;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 public class PurseRechargeActivity extends BaseActivity implements View.OnClickListener {
+
+    public class TimerExample {
+        private static final int INTERVAL = 1; // 1s
+        private static final int DURATION = 60; // 60s
+        private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        public int elapsedTime = 0;
+        public boolean isRunning;
+
+        public void start() {
+            if (scheduler.isShutdown() || scheduler.isTerminated()) {
+                scheduler = Executors.newScheduledThreadPool(1);
+            }
+            scheduler.scheduleWithFixedDelay(() -> {
+                if (elapsedTime < DURATION) {
+                    isRunning = true;
+                    elapsedTime += INTERVAL;
+                } else {
+                    scheduler.shutdown();
+                    isRunning = false;
+                    elapsedTime = 0;
+                }
+            }, 0, INTERVAL, TimeUnit.SECONDS);
+        }
+    }
     ActivityMinePurseRechargeBinding binding;
     String payType = "alipay";
     int _type = 0;
@@ -46,6 +73,8 @@ public class PurseRechargeActivity extends BaseActivity implements View.OnClickL
     private RecyclerView recyclerView1;
     Recharge_Adpter adpter;
     Recharge_PayType_Adpter adpter1;
+    TimerExample timerExample = new TimerExample();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -285,6 +314,15 @@ public class PurseRechargeActivity extends BaseActivity implements View.OnClickL
     void rechargeMoney(String inputMoney) {
         if (payType.equals("")) {
             return;
+        }
+
+        if (timerExample != null) {
+            if (!timerExample.isRunning) {
+                timerExample.start();
+            } else {
+                ToastUtils.toastMsg("请稍等");
+                return;
+            }
         }
         RequestParamsBean registerBean = new RequestParamsBean();
         registerBean.amount = NumberUtil.formartUploadMoney(inputMoney);
