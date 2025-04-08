7ae7a1875b97b052f31369bd1f23824380fd30e8
allen <>
2025-04-08 09:12:47 +0800

    1

--- chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/fun/page/fragment/FunOpenRedPacketFragment.java
+++ chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/fun/page/fragment/FunOpenRedPacketFragment.java
@@ -212,21 +212,33 @@ public class FunOpenRedPacketFragment extends BaseDialogFragment implements View
         }
     }
     void sendTipMsg(boolean isGroup) {
-        IMMessage msg = MessageBuilder.createTipMessage(groupId, isGroup ? SessionTypeEnum.Team :SessionTypeEnum.P2P);
-        CustomMsgBean msgBean = new CustomMsgBean();
-        msgBean.receiveUserId = DataUtil.getUserid();
-        msgBean.receiveUserName = DataUtil.getUserInfo().username;
-        msgBean.sendUserId = sendBean.result.fromUserId;
-        msgBean.sendUserName = sendBean.result.sendName;
-        if (msgBean.sendUserId == null || msgBean.sendUserId.isEmpty()) {
-            msgBean.sendUserId = openReusltBean.sendUserId;
-            msgBean.sendUserName = openReusltBean.sendUserName;
+        boolean isExit = false;
+        if (openReusltBean != null && !openReusltBean.vos.isEmpty()) {
+            for (CustomMsgBean bean :
+                    openReusltBean.vos) {
+                if (bean.userId.equals(DataUtil.getUserid())) {
+                    isExit = true;
+                    break;
+                }
+            }
         }
-        msg.setContent(new Gson().toJson(msgBean));
-        CustomMessageConfig messageConfig = new CustomMessageConfig();
-        messageConfig.enableUnreadCount = false;
-        msg.setConfig(messageConfig);
-        ChatRepo.sendMessage(msg, null);
+        if (isExit) {
+            IMMessage msg = MessageBuilder.createTipMessage(groupId, isGroup ? SessionTypeEnum.Team : SessionTypeEnum.P2P);
+            CustomMsgBean msgBean = new CustomMsgBean();
+            msgBean.receiveUserId = DataUtil.getUserid();
+            msgBean.receiveUserName = DataUtil.getUserInfo().username;
+            msgBean.sendUserId = sendBean.result.fromUserId;
+            msgBean.sendUserName = sendBean.result.sendName;
+            if (msgBean.sendUserId == null || msgBean.sendUserId.isEmpty()) {
+                msgBean.sendUserId = openReusltBean.sendId;
+                msgBean.sendUserName = openReusltBean.sendName;
+            }
+            msg.setContent(new Gson().toJson(msgBean));
+            CustomMessageConfig messageConfig = new CustomMessageConfig();
+            messageConfig.enableUnreadCount = false;
+            msg.setConfig(messageConfig);
+            ChatRepo.sendMessage(msg, null);
+        }
 //        updateMessage();
     }
 
