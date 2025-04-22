b45b133a2663718b990123a0394650d2f26210e0
allen <>
2025-04-22 16:12:43 +0800

    1

--- app/src/main/java/com/turunsi/yaoxin/main/mine/purse/recharge/PurseRechargeActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/main/mine/purse/recharge/PurseRechargeActivity.java
@@ -76,7 +76,7 @@ public class PurseRechargeActivity extends BaseActivity implements View.OnClickL
         recyclerView.setLayoutManager(gridLayoutManager);
         adpter = new Recharge_Adpter();
         List<String> list = new ArrayList<>();
-        list.add("100");
+//        list.add("100");
         list.add("200");
         list.add("300");
         list.add("500");
@@ -87,7 +87,7 @@ public class PurseRechargeActivity extends BaseActivity implements View.OnClickL
         list.add("50000");
         list.add("100000");
         adpter.setItems(list);
-        adpter.selectStr = "100";
+        adpter.selectStr = "200";
         binding.activityMinePurseRechargeDetailTv.setText("≈" + adpter.selectStr+"CNY");
         recyclerView.setAdapter(adpter);
         adpter.notifyDataSetChanged();
