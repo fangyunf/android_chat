be61752cec744dcd8c28fed45c466a6b31dab2d6
allen <>
2025-05-06 16:06:20 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.14"
+        versionName = "1.0.15"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "yitaogo"
+rootProject.name = "xiaohaitun"
 include(":app")
 include(":conversationkit-ui")
 include(":teamkit-ui")
