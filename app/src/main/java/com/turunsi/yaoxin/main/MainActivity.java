8e958f927f06dcd18184f8f585ea4ac0d97bcd6f
allen <>
2025-05-15 11:10:18 +0800

    1

--- app/src/main/java/com/turunsi/yaoxin/main/MainActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/main/MainActivity.java
@@ -238,22 +238,22 @@ public class MainActivity extends BaseActivity {
 
     }
     private void initData() {
-        ChatConfigManager.showReadStatus = false;
-//        SettingRepo.getShowReadStatus(
-//                new FetchCallback<Boolean>() {
-//                    @Override
-//                    public void onSuccess(@Nullable Boolean param) {
-//                        ChatConfigManager.showReadStatus = false;
-//                    }
-//
-//                    @Override
-//                    public void onFailed(int code) {
-//                    }
-//
-//                    @Override
-//                    public void onException(@Nullable Throwable exception) {
-//                    }
-//                });
+//        ChatConfigManager.showReadStatus = false;
+        SettingRepo.getShowReadStatus(
+                new FetchCallback<Boolean>() {
+                    @Override
+                    public void onSuccess(@Nullable Boolean param) {
+                        ChatConfigManager.showReadStatus = false;
+                    }
+
+                    @Override
+                    public void onFailed(int code) {
+                    }
+
+                    @Override
+                    public void onException(@Nullable Throwable exception) {
+                    }
+                });
     }
 
     @Override
