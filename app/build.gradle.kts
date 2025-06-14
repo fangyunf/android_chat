8bb8351321c8d71093f6ecfeeeb92cc2b0272fa1
allen <>
2025-06-14 10:02:32 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         }
     }
     defaultConfig {
-        applicationId = "com.turunsi.junshang"
+        applicationId = "com.turunsi.xiangyue"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
--- appbase/src/main/java/com/yaoxin/appbase/utils/UploadUtil.java
+++ appbase/src/main/java/com/yaoxin/appbase/utils/UploadUtil.java
@@ -104,7 +104,7 @@ public class UploadUtil {
                 .countable(true)
                 .maxSelectable(1)
                 .capture(true)
-                .captureStrategy(new CaptureStrategy(true, "com.turunsi.junshang.IMKitFileProvider"))
+                .captureStrategy(new CaptureStrategy(true, "com.turunsi.xiangyue.IMKitFileProvider"))
                 .imageEngine(new GlideEngine())
                 .forResult(Constant.REQUEST_CODE_CHOOSE);
 
