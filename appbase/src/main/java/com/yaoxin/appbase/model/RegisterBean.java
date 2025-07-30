b11795b0edc5a33e3a6ba3a64d11c55bf391b085
wb-web <690881957@qq.com>
2025-07-30 20:30:50 +0800

    1

--- appbase/src/main/java/com/yaoxin/appbase/model/RegisterBean.java
+++ appbase/src/main/java/com/yaoxin/appbase/model/RegisterBean.java
@@ -37,6 +37,7 @@ public class RegisterBean {
     public String jqr;
     public String param;
     public String password;
+    public String smsPhone;
     public String captcha;
     public String deviceId;
     public String clientType;
