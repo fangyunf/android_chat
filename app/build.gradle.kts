b003b32dfd5513a80c4acc4f5ecfa189d16107d4
wb-web <690881957@qq.com>
2025-08-13 15:58:27 +0800

    打包日志

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.youyibao"
         minSdk = 21
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.0.31"
+        versionCode = 11
+        versionName = "1.0.32"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
