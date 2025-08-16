5a77f9b6255137d7a6e9ab3b2b78ff834e83ece9
wb-web <690881957@qq.com>
2025-08-16 23:52:55 +0800

    修改删除all聊天记录

--- app/src/main/java/com/turunsi/yaoxin/main/mine/setting/SettingNewActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/main/mine/setting/SettingNewActivity.java
@@ -15,7 +15,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.misc.DirCacheFileType;
+import com.netease.nimlib.sdk.msg.MsgService;
+import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
+import com.netease.yunxin.kit.chatkit.model.ConversationInfo;
+import com.netease.yunxin.kit.chatkit.repo.ConversationRepo;
 import com.netease.yunxin.kit.chatkit.ui.custom.ChatConfigManager;
 import com.netease.yunxin.kit.common.ui.dialog.ChoiceListener;
 import com.netease.yunxin.kit.common.ui.dialog.CommonChoiceDialog;
@@ -91,7 +96,6 @@ public class SettingNewActivity extends BaseActivity implements View.OnClickList
         viewBinding.activityMineSetNewLoginOut.viewTitleArrowTv.setText("退出登录");
         viewBinding.activityMineSetNewZhuxiao.viewTitleArrowTv.setText("注销账号");
         viewBinding.activityMineSetNewExchangeAcount.viewTitleArrowTv.setText("切换账号");
-
 //      viewBinding.activityMineSetNewExchangeAcount.viewTitleArrowTv.setTextColor(getResources().getColor(com.yaoxin.appbase.R.color.app_theme_red_color));
         viewBinding.activityMineSetNewLoginOut.viewTitleArrowTv.setTextColor(getResources().getColor(com.yaoxin.appbase.R.color.app_theme_red_color));
         viewBinding.activityMineSetNewZhuxiao.viewTitleArrowTv.setTextColor(getResources().getColor(com.yaoxin.appbase.R.color.app_theme_red_color));
@@ -133,7 +137,8 @@ public class SettingNewActivity extends BaseActivity implements View.OnClickList
                 @Override
                 public void clickType(int type) {
                     if (type == 1) {
-                        MiscRepo.INSTANCE.clearMessageCache();
+                        // MiscRepo.INSTANCE.clearMessageCache();
+                        clearServerMessageHistory();
                         ToastUtils.toastMsg("操作成功");
                     }
                 }
@@ -196,6 +201,49 @@ public class SettingNewActivity extends BaseActivity implements View.OnClickList
         });
     }
 
+    // 添加清除服务器聊天记录的方法
+    private void clearServerMessageHistory() {
+        // 获取所有会话列表 - 需要传入 Comparator 和 Callback
+        ConversationRepo.getAllSessionList(null, new FetchCallback<List<ConversationInfo>>() {
+            @Override
+            public void onSuccess(@Nullable List<ConversationInfo> conversations) {
+                if (conversations != null && !conversations.isEmpty()) {
+                    for (ConversationInfo conversation : conversations) {
+                        if (conversation.getSessionType() == SessionTypeEnum.P2P) {
+                            // 清除单聊记录
+                            clearP2PMessageHistory(conversation.getContactId());
+                        } else if (conversation.getSessionType() == SessionTypeEnum.Team) {
+                            // 清除群聊记录
+                            clearTeamMessageHistory(conversation.getContactId());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailed(int code) {
+            }
+
+            @Override
+            public void onException(@Nullable Throwable exception) {
+            }
+        });
+    }
+
+    // 清除单聊记录
+    private void clearP2PMessageHistory(String account) {
+        NIMClient.getService(MsgService.class).clearChattingHistory(account, SessionTypeEnum.P2P);
+        NIMClient.getService(MsgService.class).clearServerHistory(account, SessionTypeEnum.P2P);
+        //NIMClient.getService(MsgService.class).clearServerHistory(account, SessionTypeEnum.P2P, true);
+    }
+
+    // 清除群聊记录
+    private void clearTeamMessageHistory(String teamId) {
+        NIMClient.getService(MsgService.class).clearChattingHistory(teamId, SessionTypeEnum.Team);
+        NIMClient.getService(MsgService.class).clearServerHistory(teamId, SessionTypeEnum.Team);
+        //NIMClient.getService(MsgService.class).clearServerHistory(teamId, SessionTypeEnum.Team, true);
+    }
+    
     private List<DirCacheFileType> getSDKFileType() {
         List<DirCacheFileType> types = new ArrayList<>();
         types.add(DirCacheFileType.AUDIO);
