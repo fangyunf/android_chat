ae640d870b162f85c2339bf453c0f7ea4f4a0674
wb-web <690881957@qq.com>
2025-08-19 14:24:40 +0800

    升级版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.lailejiuhao"
         minSdk = 21
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.1.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
