00d4e71aa8f11e6973397f867b863e7e50ff298d
wb-web <690881957@qq.com>
2025-08-24 12:37:59 +0800

    修改宝信接口

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
     compileSdk = 33
     signingConfigs {
         create("release") {
-            keyAlias = "zhoumo"
+            keyAlias = "baoxin"
             keyPassword = "qqqq1111"
-            storeFile = file("zhoumo.jks")
+            storeFile = file("baoxin.jks")
             storePassword = "qqqq1111"
         }
     }
@@ -22,7 +22,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.16"
+        versionName = "1.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
