9d46d07cbdafcd253701270a6fea10eb269b49a8
wb-web <690881957@qq.com>
2025-09-05 03:44:34 +0800

    修改蓝思界面

---
+++ appbase/src/main/java/com/yaoxin/appbase/view/IconTitleTemplate.java
@@ -0,0 +1,53 @@
+package com.yaoxin.appbase.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.yaoxin.appbase.R;
+
+public class IconTitleTemplate extends LinearLayout {
+    public IconTitleTemplate(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, @Nullable AttributeSet attrs) {
+        // Inflate the XML layout
+        LayoutInflater.from(context).inflate(R.layout.icon_title_template, this, true);
+        TextView titleTv = findViewById(R.id.icon_title_arrow_template_title_tv);
+        ImageView imgIv = findViewById(R.id.icon_title_arrow_template_left_iv);
+        LinearLayout bgLL = findViewById(R.id.icon_title_arrow_template_ll);
+
+        // Initialize any custom views or attributes here
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.IconTitleArrowTemplate);
+            String titleTvText = a.getString(R.styleable.IconTitleArrowTemplate_titleText);
+
+            Drawable customImage = a.getDrawable(R.styleable.IconTitleArrowTemplate_leftImg);
+            int bgColor = a.getColor(R.styleable.IconTitleArrowTemplate_bgColor, Color.WHITE);
+            int textColor = a.getColor(R.styleable.IconTitleArrowTemplate_textColor, Color.BLACK);
+            a.recycle();
+
+            // Apply custom attributes to views
+            if (titleTvText != null) {
+                titleTv.setText(titleTvText);
+            }
+            // Apply custom attributes to views
+            titleTv.setTextColor(textColor);
+
+            if (customImage != null) {
+                imgIv.setImageDrawable(customImage);
+            }
+            bgLL.setBackgroundColor(bgColor);
+        }
+    }
+}
