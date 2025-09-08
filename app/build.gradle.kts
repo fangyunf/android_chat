82bd1d3d2f19ad8392bb5d7519cc487b22937256
wb-web <690881957@qq.com>
2025-09-08 14:50:48 +0800

    增加黑名单

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.baoxin"
         minSdk = 21
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
