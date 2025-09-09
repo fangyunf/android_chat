d7238f58809dbec7d598f51cb75e323b6d6cdf48
wb-web <690881957@qq.com>
2025-09-09 17:43:47 +0800

    修改宝信接口地址

--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -1,7 +1,6 @@
 package com.yaoxin.appbase.net;
 
 
-
 /**
  * Created by will
  * on 2018/5/25.
@@ -15,8 +14,8 @@ public class Constant {
 //    public static final String ENCODE_KEY = "frxsecretpadding";
 //    public static final String MSG_ENCODE_KEY = "carefullanimchat";
 
-    public static final String BASE_URL = "http://47.117.247.4:17117/";
-    public static final String BASE_URL_H5 = "http://47.117.247.4:17117";
+    public static final String BASE_URL = "http://bxinxhsg62gsh.163-yunxin-api.com:17117/";
+    public static final String BASE_URL_H5 = "http://bxinxhsg62gsh.163-yunxin-api.com:17117";
     public static final String ENCODE_KEY = "wesecretpaddingg";
     public static final String MSG_ENCODE_KEY = "wallstreetimchat";
 
