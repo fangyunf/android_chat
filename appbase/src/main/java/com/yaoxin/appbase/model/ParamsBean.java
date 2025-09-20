fdb63006670c144b01f83f6fd706ad63780d5fb9
wb-web <690881957@qq.com>
2025-09-21 03:33:26 +0800

    修改bug

--- appbase/src/main/java/com/yaoxin/appbase/model/ParamsBean.java
+++ appbase/src/main/java/com/yaoxin/appbase/model/ParamsBean.java
@@ -15,6 +15,9 @@ public class ParamsBean {
     public String absolutePathUrl;
     public String deviceId;
     public String clientType;
+
+    public int page;
+
     public ParamsBean() {
         deviceId = DeviceUtils.getDeviceId(AppProxy.getInstance().getContext());
         clientType = Constant.clientType;
