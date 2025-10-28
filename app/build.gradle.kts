e1b04790ffd1a4fc4e9c73f8019c2217fcdf75b4
wb-web <690881957@qq.com>
2025-10-28 20:55:44 +0800

    修改版本名称

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         targetSdk = 33
 //        versionCode = 3
 //        versionName = "1.0.15"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
