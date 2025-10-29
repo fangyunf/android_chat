5c9f696dd7a092d8c6a6c881f378b6bda44e6ed9
wb-web <690881957@qq.com>
2025-10-30 01:57:23 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.youyibao"
         minSdk = 21
         targetSdk = 33
-        versionCode = 12
-        versionName = "1.0.33"
+        versionCode = 13
+        versionName = "1.0.34"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
