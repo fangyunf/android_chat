d64b80baf733fdab48c94530decc8fa4aae80753
wb-web <690881957@qq.com>
2025-11-02 16:46:21 +0800

    修改名称

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.lansi"
         minSdk = 21
         targetSdk = 33
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
