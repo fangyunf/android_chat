1cd903ed11b1de9ab0abef7e53c04d23592d1264
wb-web <690881957@qq.com>
2025-11-10 14:43:47 +0800

    修改版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.longyutai"
         minSdk = 21
         targetSdk = 33
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.2"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
