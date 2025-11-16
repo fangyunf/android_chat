406a89911c18d99010086769d5982a2cf62a7c67
wb-web <690881957@qq.com>
2025-11-17 00:47:06 +0800

    修改布局

---
+++ appbase/src/main/java/com/yaoxin/appbase/view/IconTitleArrowTemplate_1.java
@@ -0,0 +1,66 @@
+package com.yaoxin.appbase.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.yaoxin.appbase.R;
+
+public class IconTitleArrowTemplate_1 extends LinearLayout {
+    public TextView rightTv;
+    public ImageView rightIv;
+    public IconTitleArrowTemplate_1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context,attrs);
+    }
+    private void init(Context context,@Nullable AttributeSet attrs) {
+        // Inflate the XML layout
+        LayoutInflater.from(context).inflate(R.layout.icon_title_arrow_template_1, this, true);
+        TextView titleTv = findViewById(R.id.icon_title_arrow_template_title_tv);
+        rightTv = findViewById(R.id.icon_title_arrow_template_right_tv);
+        rightIv = findViewById(R.id.icon_title_arrow_template_right_iv);
+        ImageView imgIv = findViewById(R.id.icon_title_arrow_template_left_iv);
+//        LinearLayout bgLL = findViewById(R.id.icon_title_arrow_template_ll);
+
+        // Initialize any custom views or attributes here
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.IconTitleArrowTemplate);
+            String titleTvText = a.getString(R.styleable.IconTitleArrowTemplate_titleText);
+            boolean hiddenIv = a.getBoolean(R.styleable.IconTitleArrowTemplate_hideImg,false);
+
+            Drawable customImage = a.getDrawable(R.styleable.IconTitleArrowTemplate_leftImg);
+//            int bgColor = a
+//                    .getColor(
+//                            R.styleable.IconTitleArrowTemplate_bgColor,
+//                            Color.WHITE);
+            int textColor = a
+                    .getColor(
+                            R.styleable.IconTitleArrowTemplate_textColor,
+                            Color.WHITE);
+            a.recycle();
+
+            // Apply custom attributes to views
+            if (titleTvText != null) {
+                titleTv.setText(titleTvText);
+            }
+            // Apply custom attributes to views
+            titleTv.setTextColor(textColor);
+
+            if (customImage != null) {
+                imgIv.setImageDrawable(customImage);
+            }
+            if (hiddenIv) {
+                imgIv.setVisibility(GONE);
+            }
+//            bgLL.setBackgroundColor(bgColor);
+        }
+    }
+}
