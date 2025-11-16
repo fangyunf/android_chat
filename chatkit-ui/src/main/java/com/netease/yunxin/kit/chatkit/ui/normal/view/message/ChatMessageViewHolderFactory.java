0dcae394a3ab4d8ce84f168c35bea1f1a692ed15
wb-web <690881957@qq.com>
2025-11-17 02:53:50 +0800

    增加转账

--- chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/normal/view/message/ChatMessageViewHolderFactory.java
+++ chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/normal/view/message/ChatMessageViewHolderFactory.java
@@ -15,13 +15,17 @@ import static com.netease.yunxin.kit.chatkit.ui.ChatMessageType.NOTICE_MESSAGE_V
 import static com.netease.yunxin.kit.chatkit.ui.ChatMessageType.RICH_TEXT_ATTACHMENT;
 import static com.netease.yunxin.kit.chatkit.ui.ChatMessageType.RedPacket_ATTACHMENT;
 import static com.netease.yunxin.kit.chatkit.ui.ChatMessageType.TIP_MESSAGE_VIEW_TYPE;
+import static com.netease.yunxin.kit.chatkit.ui.ChatMessageType.ZhuanZhang_ATTACHMENT;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
 import com.netease.yunxin.kit.chatkit.ui.IChatFactory;
 import com.netease.yunxin.kit.chatkit.ui.databinding.ChatBaseMessageViewHolderBinding;
+import com.netease.yunxin.kit.chatkit.ui.fun.view.message.viewholder.ChatZhuanZhangMessageViewHolder;
 import com.netease.yunxin.kit.chatkit.ui.model.ChatMessageBean;
 import com.netease.yunxin.kit.chatkit.ui.normal.view.message.viewholder.ChatAudioMessageViewHolder;
 import com.netease.yunxin.kit.chatkit.ui.normal.view.message.viewholder.ChatCallMessageViewHolder;
@@ -38,72 +42,76 @@ import com.netease.yunxin.kit.chatkit.ui.normal.view.message.viewholder.ChatVide
 import com.netease.yunxin.kit.chatkit.ui.view.message.viewholder.ChatBaseMessageViewHolder;
 import com.netease.yunxin.kit.chatkit.ui.view.message.viewholder.CommonBaseMessageViewHolder;
 
-/** 标准皮肤，聊天页面消息ViewHolder工厂类。 */
+/**
+ * 标准皮肤，聊天页面消息ViewHolder工厂类。
+ */
 public abstract class ChatMessageViewHolderFactory implements IChatFactory {
 
-  @Override
-  public CommonBaseMessageViewHolder createViewHolder(@NonNull ViewGroup parent, int viewType) {
+    @Override
+    public CommonBaseMessageViewHolder createViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-    CommonBaseMessageViewHolder viewHolder = createViewHolderCustom(parent, viewType);
-    if (viewHolder == null) {
-      viewHolder = getViewHolderDefault(parent, viewType);
-    }
-    return viewHolder;
-  }
-
-  public abstract @Nullable CommonBaseMessageViewHolder createViewHolderCustom(
-      @NonNull ViewGroup parent, int viewType);
-
-  protected CommonBaseMessageViewHolder getViewHolderDefault(
-      @NonNull ViewGroup parent, int viewType) {
-
-    ChatBaseMessageViewHolder viewHolder;
-    ChatBaseMessageViewHolderBinding viewHolderBinding =
-        ChatBaseMessageViewHolderBinding.inflate(
-            LayoutInflater.from(parent.getContext()), parent, false);
-    if (viewType == NORMAL_MESSAGE_VIEW_TYPE_AUDIO) {
-      viewHolder = new ChatAudioMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == NORMAL_MESSAGE_VIEW_TYPE_IMAGE) {
-      viewHolder = new ChatImageMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == NORMAL_MESSAGE_VIEW_TYPE_VIDEO) {
-      viewHolder = new ChatVideoMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == NOTICE_MESSAGE_VIEW_TYPE) {
-      viewHolder = new ChatNotificationMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == TIP_MESSAGE_VIEW_TYPE) {
-      viewHolder = new ChatTipsMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == NORMAL_MESSAGE_VIEW_TYPE_FILE) {
-      viewHolder = new ChatFileMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == LOCATION_MESSAGE_VIEW_TYPE) {
-      viewHolder = new ChatLocationMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == CALL_MESSAGE_VIEW_TYPE) {
-      viewHolder = new ChatCallMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == MULTI_FORWARD_ATTACHMENT) {
-      //custom message
-      viewHolder = new ChatForwardMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == RICH_TEXT_ATTACHMENT) {
-      viewHolder = new ChatRichTextMessageViewHolder(viewHolderBinding, viewType);
-    } else if (viewType == RedPacket_ATTACHMENT) {
-      viewHolder = new ChatRedPacketMessageViewHolder(viewHolderBinding, viewType);
-    } else {
-      //default as text message
-      viewHolder = new ChatTextMessageViewHolder(viewHolderBinding, viewType);
+        CommonBaseMessageViewHolder viewHolder = createViewHolderCustom(parent, viewType);
+        if (viewHolder == null) {
+            viewHolder = getViewHolderDefault(parent, viewType);
+        }
+        return viewHolder;
     }
 
-    return viewHolder;
-  }
+    public abstract @Nullable CommonBaseMessageViewHolder createViewHolderCustom(
+            @NonNull ViewGroup parent, int viewType);
 
-  public abstract int getCustomViewType(ChatMessageBean messageBean);
+    protected CommonBaseMessageViewHolder getViewHolderDefault(
+            @NonNull ViewGroup parent, int viewType) {
 
-  @Override
-  public int getItemViewType(ChatMessageBean messageBean) {
-    if (messageBean != null) {
-      int customViewType = getCustomViewType(messageBean);
-      if (customViewType > 0) {
-        return customViewType;
-      } else {
-        return messageBean.getViewType();
-      }
+        ChatBaseMessageViewHolder viewHolder;
+        ChatBaseMessageViewHolderBinding viewHolderBinding =
+                ChatBaseMessageViewHolderBinding.inflate(
+                        LayoutInflater.from(parent.getContext()), parent, false);
+        if (viewType == NORMAL_MESSAGE_VIEW_TYPE_AUDIO) {
+            viewHolder = new ChatAudioMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == NORMAL_MESSAGE_VIEW_TYPE_IMAGE) {
+            viewHolder = new ChatImageMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == NORMAL_MESSAGE_VIEW_TYPE_VIDEO) {
+            viewHolder = new ChatVideoMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == NOTICE_MESSAGE_VIEW_TYPE) {
+            viewHolder = new ChatNotificationMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == TIP_MESSAGE_VIEW_TYPE) {
+            viewHolder = new ChatTipsMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == NORMAL_MESSAGE_VIEW_TYPE_FILE) {
+            viewHolder = new ChatFileMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == LOCATION_MESSAGE_VIEW_TYPE) {
+            viewHolder = new ChatLocationMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == CALL_MESSAGE_VIEW_TYPE) {
+            viewHolder = new ChatCallMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == MULTI_FORWARD_ATTACHMENT) {
+            //custom message
+            viewHolder = new ChatForwardMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == RICH_TEXT_ATTACHMENT) {
+            viewHolder = new ChatRichTextMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == RedPacket_ATTACHMENT) {
+            viewHolder = new ChatRedPacketMessageViewHolder(viewHolderBinding, viewType);
+        } else if (viewType == ZhuanZhang_ATTACHMENT) {
+            viewHolder = new ChatZhuanZhangMessageViewHolder(viewHolderBinding, viewType);
+        } else {
+            //default as text message
+            viewHolder = new ChatTextMessageViewHolder(viewHolderBinding, viewType);
+        }
+
+        return viewHolder;
     }
-    return 0;
-  }
+
+    public abstract int getCustomViewType(ChatMessageBean messageBean);
+
+    @Override
+    public int getItemViewType(ChatMessageBean messageBean) {
+        if (messageBean != null) {
+            int customViewType = getCustomViewType(messageBean);
+            if (customViewType > 0) {
+                return customViewType;
+            } else {
+                return messageBean.getViewType();
+            }
+        }
+        return 0;
+    }
 }
