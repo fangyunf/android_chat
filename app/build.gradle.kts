309fdae609ffe910a701a454c5d44f6748f6aeb3
wb-web <690881957@qq.com>
2025-12-03 14:36:40 +0800

    初始化米聊

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         }
     }
     defaultConfig {
-        applicationId = "com.turunsi.mishang"
+        applicationId = "com.turunsi.miliao"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
