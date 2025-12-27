3193797dcc2a72bf0c4076725541622f5a548cfc
wb-web <690881957@qq.com>
2025-12-27 14:02:48 +0800

    隐藏注销功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.youyibao"
         minSdk = 21
         targetSdk = 33
-        versionCode = 13
-        versionName = "1.0.34"
+        versionCode = 14
+        versionName = "1.0.35"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
