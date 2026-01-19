82e5cde7425e09563ccb48b396beb66e226c4420
wb-web <690881957@qq.com>
2026-01-20 00:29:10 +0800

    修改界面

--- chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/fun/page/fragment/FunChatP2PFragment.java
+++ chatkit-ui/src/main/java/com/netease/yunxin/kit/chatkit/ui/fun/page/fragment/FunChatP2PFragment.java
@@ -12,8 +12,10 @@ import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewTreeObserver;
+
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
 import com.netease.nimlib.sdk.msg.model.IMMessage;
 import com.netease.yunxin.kit.alog.ALog;
@@ -41,218 +43,216 @@ import org.greenrobot.eventbus.ThreadMode;
  * 单聊和群聊有一些差异，为了方便维护，将差异化的部分抽象到FunChatP2PFragment和FunChatTeamFragment中
  */
 public class FunChatP2PFragment extends FunChatFragment {
-  private static final String TAG = "ChatP2PFunFragment";
+    private static final String TAG = "ChatP2PFunFragment";
 
-  private static final int TYPE_DELAY_TIME = 3000;
+    private static final int TYPE_DELAY_TIME = 3000;
 
-  public UserInfo userInfo;
+    public UserInfo userInfo;
 
-  public FriendInfo friendInfo;
+    public FriendInfo friendInfo;
 
-  public IMMessage anchorMessage;
+    public IMMessage anchorMessage;
 
-  private final Handler handler = new Handler();
+    private final Handler handler = new Handler();
 
-  private final Runnable stopTypingRunnable = () -> chatView.setTypeState(false);
+    private final Runnable stopTypingRunnable = () -> chatView.setTypeState(false);
 
-  protected Observer<IMMessageReceiptInfo> p2pReceiptObserver;
+    protected Observer<IMMessageReceiptInfo> p2pReceiptObserver;
 
-  @Override
-  protected void initData(Bundle bundle) {
-    ALog.d(LIB_TAG, TAG, "initData");
-    sessionType = SessionTypeEnum.P2P;
-    userInfo = (UserInfo) bundle.getSerializable(RouterConstant.CHAT_KRY);
-    sessionID = (String) bundle.getSerializable(RouterConstant.CHAT_ID_KRY);
-    if (userInfo == null && TextUtils.isEmpty(sessionID)) {
-      requireActivity().finish();
-      return;
+    @Override
+    protected void initData(Bundle bundle) {
+        ALog.d(LIB_TAG, TAG, "initData");
+        sessionType = SessionTypeEnum.P2P;
+        userInfo = (UserInfo) bundle.getSerializable(RouterConstant.CHAT_KRY);
+        sessionID = (String) bundle.getSerializable(RouterConstant.CHAT_ID_KRY);
+        if (userInfo == null && TextUtils.isEmpty(sessionID)) {
+            requireActivity().finish();
+            return;
+        }
+        if (TextUtils.isEmpty(sessionID)) {
+            sessionID = userInfo.getAccount();
+        }
+        anchorMessage = (IMMessage) bundle.getSerializable(RouterConstant.KEY_MESSAGE);
+        refreshView();
     }
-    if (TextUtils.isEmpty(sessionID)) {
-      sessionID = userInfo.getAccount();
-    }
-    anchorMessage = (IMMessage) bundle.getSerializable(RouterConstant.KEY_MESSAGE);
-    refreshView();
-  }
 
-  @Override
-  protected void initView() {
-    super.initView();
-    if (DataUtil.getKeFuId().equals(sessionID)) {
+    @Override
+    protected void initView() {
+        super.initView();
+        if (DataUtil.getKeFuId().equals(sessionID)) {
 
-    } else {
-
-    chatView
-        .getTitleBar()
-        .setOnBackIconClickListener(v -> requireActivity().onBackPressed())
-        .setActionImg(R.drawable.ic_more_point)
-        .setActionListener(
-            v -> {
-              chatView.hideCurrentInput();
-              XKitRouter.withKey(RouterConstant.PATH_FUN_CHAT_SETTING_PAGE)
-                  .withParam(RouterConstant.CHAT_ID_KRY, sessionID)
-                  .withContext(requireActivity())
-                  .navigate();
-            });
-    }
-    EventBus.getDefault().register(this);
-  }
-
-  @Subscribe(threadMode = ThreadMode.MAIN)
-  public void onMessageEvent(BaseEvent event) {
-    if (event.getTag().equals("clearP2PMessageList")) {
-      chatView.clearMessageList();
-    }
-  }
-
-  public void refreshView() {
-    String name = sessionID;
-    if (friendInfo != null) {
-      name = friendInfo.getName();
-    } else if (userInfo != null) {
-      name = userInfo.getName();
-    }
-    if (DataUtil.getKeFuId().equals(sessionID)) {
-      chatView.getTitleBar().setTitle("客服");
-      chatView
-              .getTitleBar().getActionImageView().setVisibility(View.GONE);
-    } else {
-      chatView.getTitleBar().setTitle(name);
-    }
-  }
-
-  @Override
-  protected void initViewModel() {
-    ALog.d(LIB_TAG, TAG, "initViewModel");
-    viewModel = new ViewModelProvider(this).get(ChatP2PViewModel.class);
-    viewModel.init(sessionID, SessionTypeEnum.P2P);
-
-    if (chatConfig != null && chatConfig.chatListener != null) {
-      chatConfig.chatListener.onSessionChange(sessionID, sessionType);
-    }
-    if (chatConfig != null && chatConfig.messageProperties != null) {
-      viewModel.setShowReadStatus(chatConfig.messageProperties.showP2pMessageStatus);
-    }
-  }
-
-  @Override
-  protected void initToFetchData() {
-    if (viewModel instanceof ChatP2PViewModel) {
-      ((ChatP2PViewModel) viewModel).getFriendInfo(sessionID);
-      viewModel.initFetch(anchorMessage, false);
-    }
-  }
-
-  @Override
-  public void onDestroyView() {
-    super.onDestroyView();
-    ((ChatP2PViewModel) viewModel).getMessageReceiptLiveData().removeObserver(p2pReceiptObserver);
-    EventBus.getDefault().unregister(this);
-  }
-
-  @Override
-  protected void initDataObserver() {
-    super.initDataObserver();
-    ALog.d(LIB_TAG, TAG, "initDataObserver");
-    p2pReceiptObserver =
-        imMessageReceiptInfo ->
+        } else {
             chatView
-                .getMessageListView()
-                .setP2PReceipt(imMessageReceiptInfo.getMessageReceipt().getTime());
-    ((ChatP2PViewModel) viewModel).getMessageReceiptLiveData().observeForever(p2pReceiptObserver);
-
-    ((ChatP2PViewModel) viewModel)
-        .getTypeStateLiveData()
-        .observe(
-            getViewLifecycleOwner(),
-            isTyping -> {
-              handler.removeCallbacks(stopTypingRunnable);
-              chatView.setTypeState(isTyping);
-              if (isTyping) {
-                handler.postDelayed(stopTypingRunnable, TYPE_DELAY_TIME);
-              }
-            });
-    ((ChatP2PViewModel) viewModel)
-        .getFriendInfoLiveData()
-        .observe(
-            getViewLifecycleOwner(),
-            result -> {
-              if (result.getLoadStatus() == LoadStatus.Success) {
-                friendInfo = result.getData();
-                if (friendInfo != null) {
-                  userInfo = friendInfo.getUserInfo();
-                }
-                refreshView();
-              }
-            });
-  }
-
-  @Override
-  public void onNewIntent(Intent intent) {
-    ALog.d(LIB_TAG, TAG, "onNewIntent");
-    anchorMessage = (IMMessage) intent.getSerializableExtra(RouterConstant.KEY_MESSAGE);
-    ChatMessageBean anchorMessageBean =
-        (ChatMessageBean) intent.getSerializableExtra(RouterConstant.KEY_MESSAGE_BEAN);
-    if (anchorMessageBean != null) {
-      anchorMessage = anchorMessageBean.getMessageData().getMessage();
-    } else if (anchorMessage != null) {
-      anchorMessageBean = new ChatMessageBean(new IMMessageInfo(anchorMessage));
+                    .getTitleBar()
+                    .setOnBackIconClickListener(v -> requireActivity().onBackPressed())
+                    .setActionImg(R.drawable.ic_more_point)
+                    .setActionListener(
+                            v -> {
+                                chatView.hideCurrentInput();
+                                XKitRouter.withKey(RouterConstant.PATH_FUN_CHAT_SETTING_PAGE)
+                                        .withParam(RouterConstant.CHAT_ID_KRY, sessionID)
+                                        .withContext(requireActivity())
+                                        .navigate();
+                            });
+        }
+        EventBus.getDefault().register(this);
     }
-    if (anchorMessage != null) {
-      int position = chatView.getMessageListView().searchMessagePosition(anchorMessage.getUuid());
-      if (position >= 0) {
-        chatView
-            .getMessageListView()
-            .getViewTreeObserver()
-            .addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener() {
-                  @Override
-                  public void onGlobalLayout() {
-                    chatView
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(BaseEvent event) {
+        if (event.getTag().equals("clearP2PMessageList")) {
+            chatView.clearMessageList();
+        }
+    }
+
+    public void refreshView() {
+        String name = sessionID;
+        if (friendInfo != null) {
+            name = friendInfo.getName();
+        } else if (userInfo != null) {
+            name = userInfo.getName();
+        }
+        if (DataUtil.getKeFuId().equals(sessionID)) {
+            chatView.getTitleBar().setTitle("客服");
+            chatView.getTitleBar().getActionImageView().setVisibility(View.GONE);
+        } else {
+            chatView.getTitleBar().setTitle(name);
+        }
+    }
+
+    @Override
+    protected void initViewModel() {
+        ALog.d(LIB_TAG, TAG, "initViewModel");
+        viewModel = new ViewModelProvider(this).get(ChatP2PViewModel.class);
+        viewModel.init(sessionID, SessionTypeEnum.P2P);
+
+        if (chatConfig != null && chatConfig.chatListener != null) {
+            chatConfig.chatListener.onSessionChange(sessionID, sessionType);
+        }
+        if (chatConfig != null && chatConfig.messageProperties != null) {
+            viewModel.setShowReadStatus(chatConfig.messageProperties.showP2pMessageStatus);
+        }
+    }
+
+    @Override
+    protected void initToFetchData() {
+        if (viewModel instanceof ChatP2PViewModel) {
+            ((ChatP2PViewModel) viewModel).getFriendInfo(sessionID);
+            viewModel.initFetch(anchorMessage, false);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ((ChatP2PViewModel) viewModel).getMessageReceiptLiveData().removeObserver(p2pReceiptObserver);
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    protected void initDataObserver() {
+        super.initDataObserver();
+        ALog.d(LIB_TAG, TAG, "initDataObserver");
+        p2pReceiptObserver =
+                imMessageReceiptInfo ->
+                        chatView
+                                .getMessageListView()
+                                .setP2PReceipt(imMessageReceiptInfo.getMessageReceipt().getTime());
+        ((ChatP2PViewModel) viewModel).getMessageReceiptLiveData().observeForever(p2pReceiptObserver);
+
+        ((ChatP2PViewModel) viewModel)
+                .getTypeStateLiveData()
+                .observe(
+                        getViewLifecycleOwner(),
+                        isTyping -> {
+                            handler.removeCallbacks(stopTypingRunnable);
+                            chatView.setTypeState(isTyping);
+                            if (isTyping) {
+                                handler.postDelayed(stopTypingRunnable, TYPE_DELAY_TIME);
+                            }
+                        });
+        ((ChatP2PViewModel) viewModel)
+                .getFriendInfoLiveData()
+                .observe(
+                        getViewLifecycleOwner(),
+                        result -> {
+                            if (result.getLoadStatus() == LoadStatus.Success) {
+                                friendInfo = result.getData();
+                                if (friendInfo != null) {
+                                    userInfo = friendInfo.getUserInfo();
+                                }
+                                refreshView();
+                            }
+                        });
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        ALog.d(LIB_TAG, TAG, "onNewIntent");
+        anchorMessage = (IMMessage) intent.getSerializableExtra(RouterConstant.KEY_MESSAGE);
+        ChatMessageBean anchorMessageBean =
+                (ChatMessageBean) intent.getSerializableExtra(RouterConstant.KEY_MESSAGE_BEAN);
+        if (anchorMessageBean != null) {
+            anchorMessage = anchorMessageBean.getMessageData().getMessage();
+        } else if (anchorMessage != null) {
+            anchorMessageBean = new ChatMessageBean(new IMMessageInfo(anchorMessage));
+        }
+        if (anchorMessage != null) {
+            int position = chatView.getMessageListView().searchMessagePosition(anchorMessage.getUuid());
+            if (position >= 0) {
+                chatView
                         .getMessageListView()
                         .getViewTreeObserver()
-                        .removeOnGlobalLayoutListener(this);
-                    chatView
-                        .getRootView()
-                        .post(() -> chatView.getMessageListView().scrollToPosition(position));
-                  }
-                });
-        chatView.getMessageListView().scrollToPosition(position);
-      } else {
-        chatView.clearMessageList();
-        // need to add anchor message to list panel
-        chatView.appendMessage(anchorMessageBean);
-        viewModel.initFetch(anchorMessage, false);
-      }
-    }
-  }
-
-  public MessageBottomLayout getMessageBottomLayout() {
-    return viewBinding.chatView.getBottomInputLayout();
-  }
-
-  @Override
-  public String getSessionName() {
-    if (friendInfo != null) {
-      return friendInfo.getName();
-    }
-    if (userInfo != null) {
-      return userInfo.getName();
-    }
-    return super.getSessionName();
-  }
-
-  @Override
-  public void updateCurrentUserInfo() {
-    UserInfo userInfo = ChatUserCache.getUserInfo(sessionID);
-    if (userInfo != null) {
-      this.userInfo = userInfo;
+                        .addOnGlobalLayoutListener(
+                                new ViewTreeObserver.OnGlobalLayoutListener() {
+                                    @Override
+                                    public void onGlobalLayout() {
+                                        chatView
+                                                .getMessageListView()
+                                                .getViewTreeObserver()
+                                                .removeOnGlobalLayoutListener(this);
+                                        chatView
+                                                .getRootView()
+                                                .post(() -> chatView.getMessageListView().scrollToPosition(position));
+                                    }
+                                });
+                chatView.getMessageListView().scrollToPosition(position);
+            } else {
+                chatView.clearMessageList();
+                // need to add anchor message to list panel
+                chatView.appendMessage(anchorMessageBean);
+                viewModel.initFetch(anchorMessage, false);
+            }
+        }
     }
 
-    FriendInfo friendInfo = ChatUserCache.getFriendInfo(sessionID);
-    if (friendInfo != null) {
-      this.friendInfo = friendInfo;
+    public MessageBottomLayout getMessageBottomLayout() {
+        return viewBinding.chatView.getBottomInputLayout();
     }
 
-    refreshView();
-  }
+    @Override
+    public String getSessionName() {
+        if (friendInfo != null) {
+            return friendInfo.getName();
+        }
+        if (userInfo != null) {
+            return userInfo.getName();
+        }
+        return super.getSessionName();
+    }
+
+    @Override
+    public void updateCurrentUserInfo() {
+        UserInfo userInfo = ChatUserCache.getUserInfo(sessionID);
+        if (userInfo != null) {
+            this.userInfo = userInfo;
+        }
+
+        FriendInfo friendInfo = ChatUserCache.getFriendInfo(sessionID);
+        if (friendInfo != null) {
+            this.friendInfo = friendInfo;
+        }
+
+        refreshView();
+    }
 }
