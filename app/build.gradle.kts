892e498f212f403a3f17999d312c5721d4936c7f
wb-web <690881957@qq.com>
2026-01-21 01:09:49 +0800

    修改版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,10 +21,8 @@ android {
         applicationId = "com.turunsi.qianmanman"
         minSdk = 21
         targetSdk = 33
-//        versionCode = 5
-//        versionName = "1.0.4"
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.5"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
