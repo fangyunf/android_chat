4d4729645703424b6fd003d732847aab3deb2a62
wb-web <690881957@qq.com>
2026-01-23 02:07:05 +0800

    修改bug

--- app/src/main/java/com/turunsi/yaoxin/about/AboutActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/about/AboutActivity.java
@@ -5,8 +5,10 @@
 package com.turunsi.yaoxin.about;
 
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.turunsi.yaoxin.R;
 import com.turunsi.yaoxin.databinding.ActivityAboutBinding;
 import com.turunsi.yaoxin.utils.AppUtils;
@@ -14,19 +16,19 @@ import com.netease.yunxin.kit.common.ui.activities.BrowseActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
-  private ActivityAboutBinding viewBinding;
-  private final String productUrl = "https://netease.im/m/";
+    private final String productUrl = "https://netease.im/m/";
+    private ActivityAboutBinding viewBinding;
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    viewBinding = ActivityAboutBinding.inflate(getLayoutInflater());
-    setContentView(viewBinding.getRoot());
-    viewBinding.flProduct.setOnClickListener(
-        v ->
-            BrowseActivity.Companion.launch(
-                AboutActivity.this, getString(R.string.mine_about), productUrl));
-    viewBinding.tvVersion.setText(AppUtils.getAppVersionName(this));
-    viewBinding.aboutTitleBar.setOnBackIconClickListener(v -> onBackPressed());
-  }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
+        viewBinding.flProduct.setOnClickListener(
+                v ->
+                        BrowseActivity.Companion.launch(
+                                AboutActivity.this, getString(R.string.mine_about), productUrl));
+        viewBinding.tvVersion.setText(AppUtils.getAppVersionName(this));
+        viewBinding.aboutTitleBar.setOnBackIconClickListener(v -> onBackPressed());
+    }
 }
