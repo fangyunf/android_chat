ae4bee3ceecdce005e41703b5eb7d11eaec90d07
wb-web <690881957@qq.com>
2026-01-23 16:27:36 +0800

    修改bug

--- conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/ConversationCustom.java
+++ conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/ConversationCustom.java
@@ -5,11 +5,18 @@
 package com.netease.yunxin.kit.conversationkit.ui;
 
 import android.content.Context;
+import android.text.TextUtils;
 
+import com.google.gson.Gson;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.msg.MsgService;
 import com.netease.nimlib.sdk.msg.attachment.NetCallAttachment;
 import com.netease.nimlib.sdk.msg.constant.MsgTypeEnum;
+import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
+import com.netease.nimlib.sdk.msg.model.IMMessage;
 import com.netease.yunxin.kit.chatkit.model.ConversationInfo;
 import com.netease.yunxin.kit.corekit.im.model.AttachmentContent;
+import com.yaoxin.appbase.model.CustomMsgBean;
 import com.yaoxin.appbase.utils.AESUtil;
 
 public class ConversationCustom {
@@ -53,6 +60,33 @@ public class ConversationCustom {
                     String result = conversationInfo.getContent();
                     if (conversationInfo.getAttachment() instanceof AttachmentContent) {
                         result = ((AttachmentContent) conversationInfo.getAttachment()).getContent();
+                    }
+                    // 尝试从 getAttachStr() 获取数据
+                    // 使用 queryLastMessage() 查询最后一条消息
+                    try {
+                        String account = conversationInfo.getContactId();
+                        SessionTypeEnum sessionType = conversationInfo.getSessionType();
+                        if (!TextUtils.isEmpty(account) && sessionType != null) {
+                            IMMessage message = NIMClient.getService(MsgService.class).queryLastMessage(account, sessionType);
+                            if (message != null && !TextUtils.isEmpty(message.getAttachStr())) {
+                                // 从 getAttachStr() 解析数据
+                                CustomMsgBean bean = new Gson().fromJson(message.getAttachStr(), CustomMsgBean.class);
+                                if (bean != null) {
+                                    // 根据类型返回对应的文本
+                                    if (bean.type == 21) {
+                                        return "[专属红包]";
+                                    } else if (bean.type == 22) {
+                                        return "[个人红包]";
+                                    } else if (bean.type == 23) {
+                                        return "[红包]";
+                                    } else if (bean.type == 28) {
+                                        return "[转账]";
+                                    }
+                                }
+                            }
+                        }
+                    } catch (Exception e) {
+                        // 查询失败，继续使用原来的逻辑
                     }
                     return result;
                 default:
