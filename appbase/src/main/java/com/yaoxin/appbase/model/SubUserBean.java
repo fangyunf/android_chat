6aba23901b3dd50da5614467315e2bcff14272dc
wb-web <690881957@qq.com>
2026-01-26 00:39:26 +0800

    1

---
+++ appbase/src/main/java/com/yaoxin/appbase/model/SubUserBean.java
@@ -0,0 +1,9 @@
+package com.yaoxin.appbase.model;
+
+public class SubUserBean {
+    public String subId;  // 副号ID
+    public String name;   // 副号名称
+    public String avatar; // 副号头像
+    public String phone;  // 副号手机号
+    public String phoneNo;  // 副号手机号
+}
