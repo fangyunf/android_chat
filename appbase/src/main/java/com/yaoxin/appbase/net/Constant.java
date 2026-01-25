758e9f17c13affa97c37e1cb5d3e45c3f28cc0a2
wb-web <690881957@qq.com>
2026-01-26 00:21:09 +0800

    1

--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -1,7 +1,6 @@
 package com.yaoxin.appbase.net;
 
 
-
 /**
  * Created by will
  * on 2018/5/25.
@@ -17,8 +16,8 @@ public class Constant {
 
     public static final String BASE_URL = "http://youyou.chatyouyuan.com/";
     public static final String BASE_URL_H5 = "http://youyou.chatyouyuan.com";
-    public static final String ENCODE_KEY = "wesecretpaddingg";
-    public static final String MSG_ENCODE_KEY = "wallstreetimchat";
+    public static final String ENCODE_KEY = "B7cLd9gFqZpX4hVy";
+    public static final String MSG_ENCODE_KEY = "B7cLd9gFqZpX4hVy";
 
     public static final String clientType = "Android";
     public static final String RealName_Router = "RealName_Router";
