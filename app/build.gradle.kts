29e5dd1e203108cae594cbb39006814918d06647
wb-web <690881957@qq.com>
2026-02-14 23:34:10 +0800

    修改版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.guilai"
         minSdk = 21
         targetSdk = 33
-        versionCode = 13
-        versionName = "1.1.3"
+        versionCode = 14
+        versionName = "1.1.4"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
