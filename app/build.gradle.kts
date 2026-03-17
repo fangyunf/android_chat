47f0731fbc8ae55d59622e25ddd2c608ad722aa0
wb-web <690881957@qq.com>
2026-03-18 00:59:18 +0800

    修改bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.anyu"
         minSdk = 21
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 100
+        versionName = "1.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -14,9 +14,9 @@ public class Constant {
 //    public static final String ENCODE_KEY = "frxsecretpadding";
 //    public static final String MSG_ENCODE_KEY = "carefullanimchat";
 
-    public static final String BASE_URL = "http://wuyoucheng.hlydns.cn:16116/";
-    public static final String BASE_URL_H5 = "http://wuyoucheng.hlydns.cn:16116";
-    public static final String CHOUJIANG_H5 = "http://wuyoucheng.hlydns.cn:16116/gw/build/";
+    public static final String BASE_URL = "http://api.fkvpmn.cn:9968/";
+    public static final String BASE_URL_H5 = "http://api.fkvpmn.cn:9968/";
+    public static final String CHOUJIANG_H5 = "http://api.fkvpmn.cn:9968/gw/build/";
 
     //    public static final String BASE_URL = "http://47.122.86.112:10125/";
 //    public static final String BASE_URL_H5 = "http://47.122.86.112:10125";
