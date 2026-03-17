21535ecb21126c4eccaabb42723c4c827cd80f4c
wb-web <690881957@qq.com>
2026-03-18 01:11:21 +0800

    1

--- conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/page/viewmodel/ConversationViewModel.java
+++ conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/page/viewmodel/ConversationViewModel.java
@@ -203,9 +203,6 @@ public class ConversationViewModel extends BaseViewModel {
           }
       }
 
-      // 把“自己给自己”的未读数也算进单聊未读总数（用于 TabBar 角标）
-      singleChatUnreadCount += com.yaoxin.appbase.utils.AppProxy.getInstance().getSelfToSelfUnread();
-
       FetchResult<List<Integer>> fetchResult = new FetchResult<>(LoadStatus.Success);
       ArrayList<Integer> integers = new ArrayList<>();
       integers.add(singleChatUnreadCount);
