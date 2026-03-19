8ae98b19d7aace7dccfde8eb8b26027d736e4321
wb-web <690881957@qq.com>
2026-03-19 16:05:54 +0800

    1

--- appbase/src/main/java/com/yaoxin/appbase/model/RequestParams1Bean.java
+++ appbase/src/main/java/com/yaoxin/appbase/model/RequestParams1Bean.java
@@ -5,16 +5,17 @@ import com.yaoxin.appbase.utils.AppProxy;
 import com.yaoxin.appbase.utils.DeviceUtils;
 
 public class RequestParams1Bean {
-    private String phone;
+    public String phone;
     public String id;
     public String userId;
     public String zfb;
-    private String name;
+    public String name;
     public String usdt;
     public String certNo;
     public int type;
     private String deviceId;
     private String clientType;
+
     public RequestParams1Bean(String phone1, String name1, int type1) {
         deviceId = DeviceUtils.getDeviceId(AppProxy.getInstance().getContext());
         clientType = Constant.clientType;
