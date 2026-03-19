42c75097f1bd2e6d69fc77de4ab836f9e77751b9
wb-web <690881957@qq.com>
2026-03-19 15:50:32 +0800

    修改银行

--- app/src/main/java/com/turunsi/yaoxin/main/mine/purse/bankcard/adapter/BankCardListAdapter.java
+++ app/src/main/java/com/turunsi/yaoxin/main/mine/purse/bankcard/adapter/BankCardListAdapter.java
@@ -11,19 +11,31 @@ import com.chad.library.adapter4.viewholder.QuickViewHolder;
 import com.turunsi.yaoxin.R;
 import com.turunsi.yaoxin.main.mine.purse.bankcard.bean.BankCardListBean;
 import com.turunsi.yaoxin.utils.Constant;
+import com.yaoxin.appbase.model.UserBean;
 
 import java.util.List;
 
-public class BankCardListAdapter extends BaseQuickAdapter<BankCardListBean, QuickViewHolder> {
+public class BankCardListAdapter extends BaseQuickAdapter<UserBean, QuickViewHolder> {
     private static final int TYPE_ITEM = 1;
     private static final int TYPE_FOOTER = 2;
+
+    public interface OnUnbindClickListener {
+        void onUnbindClick(@NonNull UserBean item, int position);
+    }
+
+    private OnUnbindClickListener onUnbindClickListener;
+
+    public void setOnUnbindClickListener(OnUnbindClickListener listener) {
+        this.onUnbindClickListener = listener;
+    }
+
     @Override
-    protected int getItemCount(@NonNull List<? extends BankCardListBean> items) {
+    protected int getItemCount(@NonNull List<? extends UserBean> items) {
         return super.getItemCount(items) + 1;
     }
 
     @Override
-    protected int getItemViewType(int position, @NonNull List<? extends BankCardListBean> list) {
+    protected int getItemViewType(int position, @NonNull List<? extends UserBean> list) {
         if (position == list.size()) {
             return TYPE_FOOTER;
         } else {
@@ -32,7 +44,20 @@ public class BankCardListAdapter extends BaseQuickAdapter<BankCardListBean, Quic
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull QuickViewHolder quickViewHolder, int i, @Nullable BankCardListBean orderListBean) {
+    protected void onBindViewHolder(@NonNull QuickViewHolder quickViewHolder, int i, @Nullable UserBean orderListBean) {
+        if (getItemViewType(i) == TYPE_FOOTER) {
+
+        } else {
+            quickViewHolder.setText(R.id.tvName, orderListBean.name);
+            quickViewHolder.setText(R.id.tvBankNo, orderListBean.phone);
+
+            // bind unbind button
+            if (onUnbindClickListener != null) {
+                quickViewHolder.getView(R.id.btnUnbind).setOnClickListener(v -> {
+                    onUnbindClickListener.onUnbindClick(orderListBean, i);
+                });
+            }
+        }
     }
 
     @NonNull
