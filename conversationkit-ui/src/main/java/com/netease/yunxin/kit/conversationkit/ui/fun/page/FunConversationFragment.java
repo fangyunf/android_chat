ad99143d89296cbc2546a1f4aa36b9f5ab250e67
wb-web <690881957@qq.com>
2026-03-19 19:12:16 +0800

    修改用户信息bug

--- conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
+++ conversationkit-ui/src/main/java/com/netease/yunxin/kit/conversationkit/ui/fun/page/FunConversationFragment.java
@@ -28,6 +28,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.nanchen.wavesidebar.FirstLetterUtil;
 import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.RequestCallback;
 import com.netease.nimlib.sdk.msg.MessageBuilder;
@@ -71,6 +72,8 @@ import org.greenrobot.eventbus.ThreadMode;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -89,6 +92,8 @@ public class FunConversationFragment extends ConversationBaseFragment {
     private String noticeText = "";
     private RecyclerView.Adapter<?> headerAdapterRef;
     private int topIndex;
+
+    ArrayList<GroupInfoBean> mContactModels = new ArrayList<>();
 
     public FunConversationFragment() {
     }
@@ -334,6 +339,8 @@ public class FunConversationFragment extends ConversationBaseFragment {
                             if (!hasXiaoZhushou) {
                                 sendMessage(xiaozhushouId);
                             }
+
+                            loadFriendList();
                         }
 
                         @Override
@@ -343,6 +350,39 @@ public class FunConversationFragment extends ConversationBaseFragment {
                     });
         }
     }
+
+    private void loadFriendList() {
+        HttpUtil.apiW().friends_friendList(new RegisterBean()).enqueue(new CommonCallback<NetData>() {
+            @Override
+            public void Successful(Call<NetData> call, Response<NetData> response, NetData body) {
+                Type type = new TypeToken<List<GroupInfoBean>>() {
+                }.getType();
+                List<GroupInfoBean> tempList = new Gson().fromJson(body.data.toString(), type);
+                // 移除客服（使用迭代器避免 ConcurrentModificationException）
+                mContactModels = new ArrayList<>();
+                for (GroupInfoBean tempBean : tempList) {
+                    if (tempBean != null && !tempBean.userId.equals(DataUtil.getKeFuId())) {
+                        mContactModels.add(tempBean);
+                    }
+                }
+                // 排序
+                Collections.sort(mContactModels, new Comparator<GroupInfoBean>() {
+                    @Override
+                    public int compare(GroupInfoBean o1, GroupInfoBean o2) {
+                        String firstLetter = FirstLetterUtil.getFirstLetter(o1.name);
+                        String secondLetter = FirstLetterUtil.getFirstLetter(o2.name);
+                        return firstLetter.compareTo(secondLetter);
+                    }
+                });
+                DataUtil.setFriendInfoList(mContactModels);
+            }
+
+            @Override
+            public void Failure(Call<NetData> call, Throwable t) {
+            }
+        });
+    }
+
 
     void requestKefu(String kefuId) {
         RegisterBean bean = new RegisterBean();
