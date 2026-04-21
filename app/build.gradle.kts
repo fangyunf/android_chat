6baf2060bcee6e332f6b1e29cf08c9f8fd6a83de
wb-web <690881957@qq.com>
2026-04-22 01:39:41 +0800

    修改bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.sanyang"
         minSdk = 21
         targetSdk = 33
-        versionCode = 16
-        versionName = "1.1.5"
+        versionCode = 17
+        versionName = "1.1.6"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
