f7502ccff92325722baecd5d5c66907caf1fff3c
wb-web <690881957@qq.com>
2026-04-30 01:50:08 +0800

    1

--- app/src/main/java/com/turunsi/yaoxin/register/RegisterActivity.java
+++ app/src/main/java/com/turunsi/yaoxin/register/RegisterActivity.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.InputType;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.PasswordTransformationMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import com.google.gson.Gson;
 import com.netease.yunxin.kit.alog.ALog;
+import com.netease.yunxin.kit.corekit.route.XKitRouter;
 import com.turunsi.yaoxin.utils.IMUtil;
 import com.yaoxin.appbase.activity.BaseActivity;
 import com.turunsi.yaoxin.databinding.ActivityRegisterBinding;
@@ -35,39 +42,62 @@ import retrofit2.Response;
 public class RegisterActivity extends BaseActivity implements View.OnClickListener {
     private Handler handler;
     ActivityRegisterBinding binding;
+    boolean isAgree = false;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        _initTfText();
 
         binding.registerActivityBackIv.setOnClickListener(this);
+        binding.registerActivityIsAgreeLl.setOnClickListener(this);
+
+        // 设置协议文本的SpannableString
+        setupAgreementText();
+        // 初始化协议复选框为选中状态
+        _initTfText();
     }
 
     void _initTfText() {
-        binding.registerActivityPhoneTf.viewTitleTfTv.setText("手机号");
-        binding.registerActivityCodeTf.viewTitleTfTv.setText("验证码");
-        binding.registerActivityPwdTf.viewTitleTfTv.setText("密码");
-        binding.registerActivityPwd2Tf.viewTitleTfTv.setText("确认密码");
+        // 设置标题
+        binding.registerActivityPhoneTf.viewTitleTfCountTitleTv.setText("请输入11位手机号码");
+        binding.registerActivityCodeTf.viewTitleTfCountTitleTv.setText("请输入验证码");
+        binding.registerActivityPwdTf.viewTitleTfCountTitleTv.setText("请输入8-12位密码,数字+字母");
+        binding.registerActivityPwd2Tf.viewTitleTfCountTitleTv.setText("请再次输入8-12位密码,数字+字母");
 
-        binding.registerActivityPhoneTf.viewTitleTfEt.setInputType(InputType.TYPE_CLASS_NUMBER);
-        binding.registerActivityCodeTf.viewTitleTfEt.setInputType(InputType.TYPE_CLASS_NUMBER);
+        // 设置输入类型
+        binding.registerActivityPhoneTf.viewTitleTfCountEt.setInputType(InputType.TYPE_CLASS_NUMBER);
+        binding.registerActivityCodeTf.viewTitleTfCountEt.setInputType(InputType.TYPE_CLASS_NUMBER);
 
-        binding.registerActivityPhoneTf.viewTitleTfEt.setHint("请输入手机号");
-        binding.registerActivityCodeTf.viewTitleTfEt.setHint("请输入验证码");
-        binding.registerActivityPwdTf.viewTitleTfEt.setHint("请输入密码");
-        binding.registerActivityPwd2Tf.viewTitleTfEt.setHint("请输入密码");
+        // 设置提示文本（根据图2）
+        binding.registerActivityPhoneTf.viewTitleTfCountEt.setHint("");
+        binding.registerActivityCodeTf.viewTitleTfCountEt.setHint("");
+        binding.registerActivityPwdTf.viewTitleTfCountEt.setHint("");
+        binding.registerActivityPwd2Tf.viewTitleTfCountEt.setHint("");
+
         binding.activityRegisterBtn.setOnClickListener(this);
 
-        binding.registerActivityCodeTf.btnCaptcha.setVisibility(View.VISIBLE);
-        CountDownView mCountDownView = binding.registerActivityCodeTf.btnCaptcha;
-        mCountDownView.setUserEdit(binding.registerActivityPhoneTf.viewTitleTfEt);
+        // 设置密码字段的可见性切换
+        binding.registerActivityPwdTf.viewTitleTfCountEyeRl.setVisibility(View.VISIBLE);
+        binding.registerActivityPwdTf.viewTitleTfCountEyeRl.setOnClickListener(this);
+        binding.registerActivityPwdTf.viewTitleTfCountEyeIv.setSelected(true);
+        binding.registerActivityPwdTf.viewTitleTfCountEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        binding.registerActivityPwd2Tf.viewTitleTfCountEyeRl.setVisibility(View.VISIBLE);
+        binding.registerActivityPwd2Tf.viewTitleTfCountEyeRl.setOnClickListener(this);
+        binding.registerActivityPwd2Tf.viewTitleTfCountEyeIv.setSelected(true);
+        binding.registerActivityPwd2Tf.viewTitleTfCountEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        // 设置验证码按钮
+        binding.registerActivityCodeTf.viewTitleTfCountCaptcha.setVisibility(View.VISIBLE);
+        CountDownView mCountDownView = binding.registerActivityCodeTf.viewTitleTfCountCaptcha;
+        mCountDownView.setUserEdit(binding.registerActivityPhoneTf.viewTitleTfCountEt);
         mCountDownView.setCountDownTime(60);
         mCountDownView.setCaptchaListener(new LoginLoader.CaptchaListener() {
             @Override
             public void onPre() {
-                String phone = getTextStr(binding.registerActivityPhoneTf.viewTitleTfEt);
+                String phone = getTextStr(binding.registerActivityPhoneTf.viewTitleTfCountEt);
                 CommonNetUtil.getPhoneCode(phone);
             }
 
@@ -77,24 +107,106 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
         });
     }
 
+    /**
+     * 设置协议文本的SpannableString，使《用户协议》和《隐私政策》可点击并显示为绿色
+     */
+    private void setupAgreementText() {
+        String text = "我已阅读并同意《用户协议》、《隐私政策》";
+        SpannableString spannableString = new SpannableString(text);
+
+        // 获取主题绿色
+        int greenColor = getResources().getColor(com.yaoxin.appbase.R.color.app_theme_color);
+
+        // 设置《用户协议》的颜色和点击事件
+        int userAgreementStart = text.indexOf("《用户协议》");
+        int userAgreementEnd = userAgreementStart + "《用户协议》".length();
+        spannableString.setSpan(new ForegroundColorSpan(greenColor),
+                userAgreementStart, userAgreementEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                XKitRouter.withKey(Constant.BaseWebViewActivityKey)
+                        .withParam("type", "2")
+                        .withParam("title", "用户协议")
+                        .withContext(RegisterActivity.this)
+                        .navigate();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(greenColor);
+                ds.setUnderlineText(false);
+            }
+        }, userAgreementStart, userAgreementEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // 设置《隐私政策》的颜色和点击事件
+        int privacyPolicyStart = text.indexOf("《隐私政策》");
+        int privacyPolicyEnd = privacyPolicyStart + "《隐私政策》".length();
+        spannableString.setSpan(new ForegroundColorSpan(greenColor),
+                privacyPolicyStart, privacyPolicyEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                XKitRouter.withKey(Constant.BaseWebViewActivityKey)
+                        .withParam("type", "1")
+                        .withParam("title", "隐私政策")
+                        .withContext(RegisterActivity.this)
+                        .navigate();
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(greenColor);
+                ds.setUnderlineText(false);
+            }
+        }, privacyPolicyStart, privacyPolicyEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // 设置TextView
+        binding.registerActivityIsCheckedTxt.setText(spannableString);
+        binding.registerActivityIsCheckedTxt.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
+    }
+
     @Override
     public void onClick(View v) {
         if (v == binding.registerActivityBackIv) {
             finish();
+        } else if (v == binding.registerActivityIsAgreeLl) {
+            binding.registerActivityIsCheckedIv.setSelected(!binding.registerActivityIsCheckedIv.isSelected());
+            isAgree = binding.registerActivityIsCheckedIv.isSelected();
+        } else if (v == binding.registerActivityPwdTf.viewTitleTfCountEyeRl) {
+            binding.registerActivityPwdTf.viewTitleTfCountEyeIv.setSelected(!binding.registerActivityPwdTf.viewTitleTfCountEyeIv.isSelected());
+            if (binding.registerActivityPwdTf.viewTitleTfCountEyeIv.isSelected()) {
+                binding.registerActivityPwdTf.viewTitleTfCountEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            } else {
+                binding.registerActivityPwdTf.viewTitleTfCountEt.setTransformationMethod(null);
+            }
+        } else if (v == binding.registerActivityPwd2Tf.viewTitleTfCountEyeRl) {
+            binding.registerActivityPwd2Tf.viewTitleTfCountEyeIv.setSelected(!binding.registerActivityPwd2Tf.viewTitleTfCountEyeIv.isSelected());
+            if (binding.registerActivityPwd2Tf.viewTitleTfCountEyeIv.isSelected()) {
+                binding.registerActivityPwd2Tf.viewTitleTfCountEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            } else {
+                binding.registerActivityPwd2Tf.viewTitleTfCountEt.setTransformationMethod(null);
+            }
         } else if (v == binding.activityRegisterBtn) {
+            if (!binding.registerActivityIsCheckedIv.isSelected()) {
+                ToastUtils.toastMsg("请同意协议");
+                return;
+            }
 
-            String phone = getTextStr(binding.registerActivityPhoneTf.viewTitleTfEt);
+            String phone = getTextStr(binding.registerActivityPhoneTf.viewTitleTfCountEt);
             if (phone.length() != 11) {
                 ToastUtils.toastMsg("手机格式错误");
                 return;
             }
-            String code = getTextStr(binding.registerActivityCodeTf.viewTitleTfEt);
+            String code = getTextStr(binding.registerActivityCodeTf.viewTitleTfCountEt);
             if (code.length() > 6) {
                 ToastUtils.toastMsg("验证码错误");
                 return;
             }
-            String pwd1 = getTextStr(binding.registerActivityPwdTf.viewTitleTfEt);
-            String pwd2 = getTextStr(binding.registerActivityPwd2Tf.viewTitleTfEt);
+            String pwd1 = getTextStr(binding.registerActivityPwdTf.viewTitleTfCountEt);
+            String pwd2 = getTextStr(binding.registerActivityPwd2Tf.viewTitleTfCountEt);
             if (!pwd1.isEmpty() && !pwd2.isEmpty() && !pwd1.equals(pwd2)) {
                 ToastUtils.toastMsg("两次密码不相同");
                 return;
@@ -111,10 +223,10 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                     .enqueue(new CommonCallback<NetData>() {
                         @Override
                         public void Successful(Call<NetData> call, Response<NetData> response, NetData body) {
-                            UserBean userBean = new Gson().fromJson((String) body.data,UserBean.class);
+                            UserBean userBean = new Gson().fromJson((String) body.data, UserBean.class);
                             DataUtil.putUserInfo(userBean);
                             DataUtil.putToken(userBean.token);
-                            IMUtil.loginIM(that,userBean.userId,userBean.imToken);
+                            IMUtil.loginIM(that, userBean.userId, userBean.imToken);
 
                         }
 
