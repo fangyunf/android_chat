90828c20df0d09f601a135db0ff6f3ea372eb42e
wb-web <690881957@qq.com>
2026-05-13 00:48:29 +0800

    修改主题色

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.yueyouping"
         minSdk = 21
         targetSdk = 33
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 100
+        versionName = "1.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
