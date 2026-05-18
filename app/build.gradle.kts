e1758fc393b2889fee0e6b10cbb1dc7893a4b4d9
wb-web <690881957@qq.com>
2026-05-18 19:20:37 +0800

    修改bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.yueyouping"
         minSdk = 21
         targetSdk = 33
-        versionCode = 106
-        versionName = "1.0.6"
+        versionCode = 107
+        versionName = "1.0.7"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
