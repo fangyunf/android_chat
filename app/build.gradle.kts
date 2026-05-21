2c5f5951c57440be6b15283fe1660f1039383857
wb-web <690881957@qq.com>
2026-05-21 18:06:22 +0800

    修改禁言

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.yueyouping"
         minSdk = 21
         targetSdk = 33
-        versionCode = 109
-        versionName = "1.0.9"
+        versionCode = 110
+        versionName = "1.1.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
