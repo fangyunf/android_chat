afb3c96b0437ec503dade318dfeafbc50e803512
wb-web <690881957@qq.com>
2026-07-13 16:33:38 +0800

    修改颜色bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.xinda"
         minSdk = 21
         targetSdk = 33
-        versionCode = 113
-        versionName = "1.1.3"
+        versionCode = 114
+        versionName = "1.1.4"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
