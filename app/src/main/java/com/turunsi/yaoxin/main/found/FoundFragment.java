0b6996382857f0c2f6fe882c308f5a028c19a164
wb-web <690881957@qq.com>
2026-07-19 01:40:54 +0800

    修改界面

--- app/src/main/java/com/turunsi/yaoxin/main/found/FoundFragment.java
+++ app/src/main/java/com/turunsi/yaoxin/main/found/FoundFragment.java
@@ -9,9 +9,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.chad.library.adapter4.BaseQuickAdapter;
 import com.netease.yunxin.kit.common.ui.fragments.BaseFragment;
+import com.netease.yunxin.kit.common.utils.SizeUtils;
+import com.netease.yunxin.kit.corekit.im.utils.RouterConstant;
+import com.netease.yunxin.kit.corekit.route.XKitRouter;
 import com.turunsi.yaoxin.R;
 import com.turunsi.yaoxin.databinding.FragmentFoundBinding;
+import com.yaoxin.appbase.utils.BarUtils;
+import com.yaoxin.appbase.utils.BaseEvent;
+import com.yaoxin.appbase.utils.StatusBarUtils;
+import com.yaoxin.appbase.utils.ToastUtils;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,14 +40,45 @@ public class FoundFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        StatusBarUtils.setStatusBarLightMode(getActivity(), true, true);
+        ViewGroup.MarginLayoutParams layoutParams =
+                (ViewGroup.MarginLayoutParams) binding.foundFragmentTopLl.getLayoutParams();
+        layoutParams.topMargin = BarUtils.getStatusBarHeight() + SizeUtils.dp2px(20);
+        binding.foundFragmentTopLl.setLayoutParams(layoutParams);
         initMenuList();
     }
 
     private void initMenuList() {
         FoundMenuAdapter adapter = new FoundMenuAdapter();
         adapter.submitList(buildMenuItems());
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener<FoundMenuItem>() {
+            @Override
+            public void onClick(@NonNull BaseQuickAdapter<FoundMenuItem, ?> baseQuickAdapter, @NonNull View view, int i) {
+                FoundMenuItem item = baseQuickAdapter.getItem(i);
+                if (item == null || item.type != FoundMenuItem.TYPE_MENU) {
+                    return;
+                }
+                handleMenuClick(item);
+            }
+        });
         binding.foundFragmentRv.setLayoutManager(new LinearLayoutManager(requireContext()));
         binding.foundFragmentRv.setAdapter(adapter);
+    }
+
+    private void handleMenuClick(FoundMenuItem item) {
+        switch (item.action) {
+            case FoundMenuItem.ACTION_SCAN:
+                EventBus.getDefault().post(new BaseEvent("gotoScan"));
+                break;
+            case FoundMenuItem.ACTION_ADD_FRIEND:
+                XKitRouter.withKey(RouterConstant.PATH_FUN_ADD_FRIEND_PAGE)
+                        .withContext(requireContext())
+                        .navigate();
+                break;
+            default:
+                ToastUtils.toastMsg("敬请期待,等待开发");
+                break;
+        }
     }
 
     /**
@@ -60,11 +101,11 @@ public class FoundFragment extends BaseFragment {
         items.add(FoundMenuItem.menu("视频号", R.mipmap.found_icon_channels));
         items.add(FoundMenuItem.menu("直播", R.mipmap.found_icon_live));
         items.add(FoundMenuItem.gap());
-        items.add(FoundMenuItem.menu("扫一扫", R.mipmap.found_icon_scan));
+        items.add(FoundMenuItem.menu("扫一扫", R.mipmap.found_icon_scan, FoundMenuItem.ACTION_SCAN));
         items.add(FoundMenuItem.menu("听一听", R.mipmap.found_icon_listen));
         items.add(FoundMenuItem.gap());
         items.add(FoundMenuItem.menu("看一看", R.mipmap.found_icon_look));
-        items.add(FoundMenuItem.menu("搜一搜", R.mipmap.found_icon_search));
+        items.add(FoundMenuItem.menu("搜一搜", R.mipmap.found_icon_search, FoundMenuItem.ACTION_ADD_FRIEND));
         items.add(FoundMenuItem.gap());
         items.add(FoundMenuItem.menu("附近的人", R.mipmap.found_icon_nearby));
         items.add(FoundMenuItem.gap());
--- app/src/main/java/com/turunsi/yaoxin/main/found/FoundMenuItem.java
+++ app/src/main/java/com/turunsi/yaoxin/main/found/FoundMenuItem.java
@@ -5,21 +5,31 @@ public class FoundMenuItem {
     public static final int TYPE_MENU = 0;
     public static final int TYPE_GAP = 1;
 
+    public static final int ACTION_DEFAULT = 0;
+    public static final int ACTION_SCAN = 1;
+    public static final int ACTION_ADD_FRIEND = 2;
+
     public final int type;
     public final String title;
     public final int iconRes;
+    public final int action;
 
-    private FoundMenuItem(int type, String title, int iconRes) {
+    private FoundMenuItem(int type, String title, int iconRes, int action) {
         this.type = type;
         this.title = title;
         this.iconRes = iconRes;
+        this.action = action;
     }
 
     public static FoundMenuItem menu(String title, int iconRes) {
-        return new FoundMenuItem(TYPE_MENU, title, iconRes);
+        return new FoundMenuItem(TYPE_MENU, title, iconRes, ACTION_DEFAULT);
+    }
+
+    public static FoundMenuItem menu(String title, int iconRes, int action) {
+        return new FoundMenuItem(TYPE_MENU, title, iconRes, action);
     }
 
     public static FoundMenuItem gap() {
-        return new FoundMenuItem(TYPE_GAP, null, 0);
+        return new FoundMenuItem(TYPE_GAP, null, 0, ACTION_DEFAULT);
     }
 }
