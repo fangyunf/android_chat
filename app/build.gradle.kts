f40c55ab7bf4567c4b985335ff61cb4084eabea1
wb-web <690881957@qq.com>
2026-08-01 18:13:55 +0800

    修改图片

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.weiliao"
         minSdk = 21
         targetSdk = 33
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 9
+        versionName = "1.0.8"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
