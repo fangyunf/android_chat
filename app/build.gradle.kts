84e8605cdf61219a2ec2e9144a9ab9ac0be08044
wb-web <690881957@qq.com>
2026-09-24 15:13:14 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.turunsi.mastercard"
         minSdk = 21
         targetSdk = 33
-        versionCode = 20
-        versionName = "1.2.0"
+        versionCode = 1
+        versionName = "1.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
+++ appbase/src/main/java/com/yaoxin/appbase/net/Constant.java
@@ -14,9 +14,9 @@ public class Constant {
 //    public static final String ENCODE_KEY = "frxsecretpadding";
 //    public static final String MSG_ENCODE_KEY = "carefullanimchat";
 
-    public static final String BASE_URL = "http://sanyang117xk.jpjsshop.com:17117/";
-    public static final String BASE_URL_H5 = "http://sanyang117xk.jpjsshop.com:17117";
-    public static final String CHOUJIANG_H5 = "http://sanyang117xk.jpjsshop.com:17117/gw/build/";
+    public static final String BASE_URL = "http://mcchatkkk925.chengsuyun.com:9995/";
+    public static final String BASE_URL_H5 = "http://mcchatkkk925.chengsuyun.com:9995";
+    public static final String CHOUJIANG_H5 = "http://mcchatkkk925.chengsuyun.com:9995/gw/build/";
 
     //    public static final String BASE_URL = "http://47.122.86.112:10125/";
 //    public static final String BASE_URL_H5 = "http://47.122.86.112:10125";
